fdfef44a19c0ac4223d9c3a0127129be29365aa0
Lukas Weber <dev@sakul-flee.de>
2023-08-25 16:41:55 +0200

    Added AppCycleCounter

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,4 +1,4 @@
-use std::{sync::Arc, time::Instant};
+use std::{sync::Arc};
 use wgpu::{
     Color, CommandEncoderDescriptor, LoadOp, Operations, RenderPassColorAttachment,
     RenderPassDescriptor, TextureViewDescriptor,
@@ -14,21 +14,18 @@ use crate::{
     engine::Engine,
 };
 
-use self::app_context::AppContext;
+use self::{app_context::AppContext, app_cycle_counter::AppCycleCounter};
 
 pub mod app_config;
 pub mod app_context;
 pub mod app_window;
+pub mod app_cycle_counter;
 
 pub struct App {
     app_config: AppConfig,
-    event_loop: EventLoop<()>,
-    window: Arc<AppWindow>,
-    engine: Arc<Engine>,
-    should_run: bool,
-    fps: u32,
-    delta_time: f64,
+    app_cycle_counter: AppCycleCounter,
     app_context: AppContext,
+    should_run: bool,
 }
 
 impl App {
@@ -46,26 +43,6 @@ impl App {
     }
 
     pub async fn from_app_config(app_config: AppConfig) -> Self {
-        let event_loop = EventLoop::new();
-
-        let fullscreen = match &app_config.monitor_config {
-            Some(x) => Some(x.fullscreen.to_winit_fullscreen(&event_loop, &x)),
-            None => None,
-        };
-
-        let size = app_config.window_config.to_physical_size();
-        let window = Arc::new(AppWindow::build_and_open(
-            "WGPU",
-            size,
-            false,
-            true,
-            fullscreen,
-            &event_loop,
-        ));
-
-        let engine = Arc::new(Engine::initialize(window.clone()).await);
-        engine.configure_surface();
-
         let mut app_context = AppContext::default();
         app_context.clear_colour = Color {
             r: 0.0,
@@ -78,26 +55,42 @@ impl App {
 
         Self {
             app_config,
-            event_loop,
-            window,
-            engine,
-            should_run: false,
-            fps: 0,
-            delta_time: 0.0,
+            app_cycle_counter: AppCycleCounter::default(),
             app_context,
+            should_run: false,
         }
     }
 
-    pub fn start(mut self) {
+    pub async fn hijack_thread_and_run(mut self) {
+        // Set `should_run` to true which will act as an exit condition
         self.should_run = true;
 
-        // << FPS Calculation >>
-        // Last "now time"
-        let mut last_cycle_time = Instant::now();
-        // Iteration count per cycle
-        let mut cycle_count: u32 = 0;
+        // Event Loop
+        let event_loop = EventLoop::new();
 
-        self.event_loop.run(move |event, _target, control_flow| {
+        // Window creation
+        let fullscreen = match &self.app_config.monitor_config {
+            Some(x) => Some(x.fullscreen.to_winit_fullscreen(&event_loop, &x)),
+            None => None,
+        };
+        let size = self.app_config.window_config.to_physical_size();
+        let window = Arc::new(AppWindow::build_and_open(
+            "WGPU",
+            size,
+            false,
+            true,
+            fullscreen,
+            &event_loop,
+        ));
+
+        // Engine creation
+        let engine = Arc::new(Engine::initialize(window.clone()).await);
+        engine.configure_surface();
+
+        // << Cycle Calculation >>
+        self.app_cycle_counter.reset();
+
+        event_loop.run(move |event, _target, control_flow| {
             // << Control Flow >>
             if self.should_run {
                 // Immediately start a new cycle once a loop is completed.
@@ -108,36 +101,18 @@ impl App {
                 *control_flow = ControlFlow::ExitWithCode(0);
             }
 
-            // << Cycle Calculation >>
-            // Increase delta count and take "now time"
-            cycle_count += 1;
-            let now_cycle_time = Instant::now();
-            // Calculate duration since last cycle time
-            let delta_duration = now_cycle_time.duration_since(last_cycle_time);
-            // Add difference to delta time
-            self.delta_time = self.delta_time + delta_duration.as_secs_f64();
+            self.handle_frame_counter();
 
-            // If delta time is over a second, end the cycle
-            if self.delta_time >= 1.0 {
-                // Update FPS counter
-                self.fps = cycle_count;
-
+            self.app_cycle_counter.tick(&(|delta_time, ups| {
                 // Update Window Title
-                self.window.get_window().set_title(&format!(
+                window.get_window().set_title(&format!(
                     "WGPU @ {} - UPS: {}/s (Δ {}s)",
-                    self.engine.get_adapter().get_info().backend.to_str(), self.fps, self.delta_time
+                    engine.get_adapter().get_info().backend.to_str(), ups, delta_time
                 ));
 
                 // Update performance outputs
-                log::debug!("UPS: {}/s (delta time: {}s)", self.fps, self.delta_time);
-
-                // One second has past, subtract that
-                self.delta_time -= 1.0;
-                // Reset cycle
-                cycle_count = 0;
-            }
-            // Update cycle time with now time
-            last_cycle_time = now_cycle_time;
+                log::debug!("UPS: {}/s (delta time: {}s)", ups, delta_time);
+            }));
 
             // << Variables >>
             let mut resize_to: Option<PhysicalSize<u32>> = None;
@@ -150,7 +125,7 @@ impl App {
 
                     // Validate that the window ID match.
                     // Should only be different if multiple windows are used.
-                    if window_id != self.window.get_window().id() {
+                    if window_id != window.get_window().id() {
                         log::warn!("Invalid window ID for 'Window Event :: Window ID: {window_id:?}, Event: {event:?}'");
                         return;
                     }
@@ -179,14 +154,13 @@ impl App {
 
                     // Validate that the window ID match.
                     // Should only be different if multiple windows are used.
-                    if window_id != self.window.get_window().id() {
+                    if window_id != window.get_window().id() {
                         log::warn!("Invalid window ID for above's Event!");
                         return;
                     }
 
                     // TODO: Rendering goes here
-                    let output_surface_texture = self
-                        .engine
+                    let output_surface_texture = engine
                         .get_surface()
                         .get_current_texture()
                         .expect("failed acquiring current texture of target window");
@@ -195,7 +169,7 @@ impl App {
                         .texture
                         .create_view(&TextureViewDescriptor::default());
 
-                    let mut command_encoder = self.engine.get_device().create_command_encoder(
+                    let mut command_encoder = engine.get_device().create_command_encoder(
                         &CommandEncoderDescriptor {
                             label: Some("Render Encoder"),
                         },
@@ -217,7 +191,7 @@ impl App {
                     let command_buffer = command_encoder.finish();
 
                     // Submit command buffer
-                    self.engine.get_queue().submit(vec![command_buffer]);
+                    engine.get_queue().submit(vec![command_buffer]);
                     output_surface_texture.present();
                 }
                 Event::RedrawEventsCleared => {
@@ -292,10 +266,10 @@ impl App {
                             self.app_config.write_to_path(&AppConfig::request_default_path());
 
                             // Skip redrawing and reconfigure the surface
-                            self.engine.configure_surface();
+                            engine.configure_surface();
                     } else {
                         // Request to redraw the next cycle
-                        self.window.get_window().request_redraw();
+                        window.get_window().request_redraw();
                     }
                 }
                 _ => (),
@@ -308,6 +282,10 @@ impl App {
         // Note: Rust thinks this line is unreachable, but it actually is.
         // #[allow(unreachable_code)]
         // return self;
+    }
+
+    fn handle_frame_counter(&mut self) {
+
     }
 
     pub fn get_app_config(&self) -> &AppConfig {
