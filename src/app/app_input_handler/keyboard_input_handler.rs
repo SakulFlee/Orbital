e587ab4e4c1db9f3c306f3fb8b042680c58750d5
Lukas Weber <dev@sakul-flee.de>
2023-09-02 14:57:50 +0200

    Bug fix: Keyboard input events would fill and not get cleared each cycle

--- src/app/app_input_handler/keyboard_input_handler.rs
+++ src/app/app_input_handler/keyboard_input_handler.rs
@@ -1,14 +1,16 @@
+use std::collections::HashSet;
+
 use winit::event::{ElementState, KeyboardInput, VirtualKeyCode};
 
-#[derive(Debug, Default, Hash)]
+#[derive(Debug, Default)]
 pub struct AppKeyboardInputHandler {
-    pressed: Vec<VirtualKeyCode>,
+    pressed: HashSet<VirtualKeyCode>,
 }
 
 impl AppKeyboardInputHandler {
     pub fn new() -> Self {
         Self {
-            pressed: Vec::new(),
+            pressed: HashSet::new(),
         }
     }
 
@@ -16,27 +18,9 @@ impl AppKeyboardInputHandler {
         if let Some(keycode) = input.virtual_keycode {
             if input.state == ElementState::Pressed {
                 // Push pressed key to vec
-                self.pressed.push(keycode);
+                self.pressed.insert(keycode);
             } else {
-                // index counter
-                let mut i = 0;
-                let mut found = false;
-                // For each pressed key ...
-                for pressed_key in self.pressed.iter() {
-                    // ... check if the keycode matches ...
-                    if *pressed_key == keycode {
-                        // ... and break the loop if found!
-                        found = true;
-                        break;
-                    }
-                    // increment index counter
-                    i += 1;
-                }
-
-                // Remove the key from the vec
-                if found {
-                    self.pressed.remove(i);
-                }
+                self.pressed.remove(&keycode);
             }
         }
     }
