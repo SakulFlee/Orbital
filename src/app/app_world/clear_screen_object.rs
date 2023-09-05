97d1408d679129cf5d7f2fb699557f9284ce52db
Lukas Weber <dev@sakul-flee.de>
2023-09-05 16:42:54 +0200

    Fixed UPS cycle

--- src/app/app_world/clear_screen_object.rs
+++ src/app/app_world/clear_screen_object.rs
@@ -3,7 +3,11 @@ use wgpu::{
     RenderPassDescriptor, TextureView,
 };
 
-use super::{object::Object, renderable::Renderable, updateable::Updateable};
+use super::{
+    object::Object,
+    renderable::Renderable,
+    updateable::{UpdateFrequency, Updateable},
+};
 
 const INCREASE_RATE: f64 = 0.005;
 
@@ -16,7 +20,12 @@ pub struct ClearScreenObject {
 impl ClearScreenObject {
     pub fn new() -> Self {
         Self {
-            clear_colour: wgpu::Color::WHITE,
+            clear_colour: wgpu::Color {
+                r: 0.0,
+                g: 0.0,
+                b: 0.0,
+                a: 1.0,
+            },
             clear_colour_index: 0,
             clear_colour_increasing: true,
         }
@@ -26,59 +35,78 @@ impl ClearScreenObject {
 impl Object for ClearScreenObject {}
 
 impl Updateable for ClearScreenObject {
-    fn update(&mut self) {
+    fn update_frequency(&self) -> UpdateFrequency {
+        UpdateFrequency::OnCycle
+    }
+
+    fn update(&mut self, delta_time: f64) {
         // Update colour variable
         match self.clear_colour_index {
             0 => {
                 if self.clear_colour_increasing {
-                    self.clear_colour.r += INCREASE_RATE;
+                    self.clear_colour.r += INCREASE_RATE * delta_time;
                 } else {
-                    self.clear_colour.r -= INCREASE_RATE;
+                    self.clear_colour.r -= INCREASE_RATE * delta_time;
                 }
 
-                if self.clear_colour.r >= 1.0 || self.clear_colour.r <= 0.0 {
-                    self.clear_colour_increasing = !self.clear_colour_increasing;
-                }
-
-                if self.clear_colour.r <= 0.1 && !self.clear_colour_increasing {
-                    self.clear_colour_index = 1;
+                if self.clear_colour.r < INCREASE_RATE {
                     self.clear_colour_increasing = true;
-                    self.clear_colour.r = 0.0;
+                    self.clear_colour_index = 1;
                 }
+
+                if self.clear_colour.r >= 1.0 {
+                    self.clear_colour_increasing = false;
+                }
+
+                // if self.clear_colour.r <= 0.1 && !self.clear_colour_increasing {
+                //     self.clear_colour_index = 1;
+                //     self.clear_colour_increasing = true;
+                //     self.clear_colour.r = 0.0;
+                // }
             }
             1 => {
                 if self.clear_colour_increasing {
-                    self.clear_colour.g += INCREASE_RATE;
+                    self.clear_colour.g += INCREASE_RATE * delta_time;
                 } else {
-                    self.clear_colour.g -= INCREASE_RATE;
+                    self.clear_colour.g -= INCREASE_RATE * delta_time;
                 }
 
-                if self.clear_colour.g >= 1.0 || self.clear_colour.g <= 0.0 {
-                    self.clear_colour_increasing = !self.clear_colour_increasing;
-                }
-
-                if self.clear_colour.g <= 0.1 && !self.clear_colour_increasing {
-                    self.clear_colour_index = 2;
+                if self.clear_colour.g < INCREASE_RATE {
                     self.clear_colour_increasing = true;
-                    self.clear_colour.g = 0.0;
+                    self.clear_colour_index = 2;
                 }
+
+                if self.clear_colour.g >= 1.0 {
+                    self.clear_colour_increasing = false;
+                }
+
+                // if self.clear_colour.g <= 0.1 && !self.clear_colour_increasing {
+                //     self.clear_colour_index = 2;
+                //     self.clear_colour_increasing = true;
+                //     self.clear_colour.g = 0.0;
+                // }
             }
             2 => {
                 if self.clear_colour_increasing {
-                    self.clear_colour.b += INCREASE_RATE;
+                    self.clear_colour.b += INCREASE_RATE * delta_time;
                 } else {
-                    self.clear_colour.b -= INCREASE_RATE;
+                    self.clear_colour.b -= INCREASE_RATE * delta_time;
                 }
 
-                if self.clear_colour.b >= 1.0 || self.clear_colour.b <= 0.0 {
-                    self.clear_colour_increasing = !self.clear_colour_increasing;
-                }
-
-                if self.clear_colour.b <= 0.1 && !self.clear_colour_increasing {
-                    self.clear_colour_index = 0;
+                if self.clear_colour.b < INCREASE_RATE {
                     self.clear_colour_increasing = true;
-                    self.clear_colour.b = 0.0;
+                    self.clear_colour_index = 0;
                 }
+
+                if self.clear_colour.b >= 1.0 {
+                    self.clear_colour_increasing = false;
+                }
+
+                // if self.clear_colour.b <= 0.1 && !self.clear_colour_increasing {
+                //     self.clear_colour_index = 0;
+                //     self.clear_colour_increasing = true;
+                //     self.clear_colour.b = 0.0;
+                // }
             }
             _ => (),
         }
--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,8 +1,6 @@
-use std::sync::Arc;
+use std::{sync::Arc, time::Instant};
 
-use wgpu::{
-    Color, TextureViewDescriptor,
-};
+use wgpu::{Color, TextureViewDescriptor};
 use winit::{
     dpi::PhysicalSize,
     event::{DeviceId, Event, KeyboardInput, VirtualKeyCode, WindowEvent},
@@ -16,23 +14,24 @@ use crate::{
 
 use self::{
     app_context::AppContext,
-    app_cycle_counter::AppCycleCounter,
     app_input_handler::{keyboard_input_handler::AppKeyboardInputHandler, AppInputHandler},
     app_world::{clear_screen_object::ClearScreenObject, AppWorld},
 };
 
 pub mod app_config;
 pub mod app_context;
-pub mod app_cycle_counter;
 pub mod app_input_handler;
 pub mod app_window;
 pub mod app_world;
 
 pub struct App {
     app_config: AppConfig,
-    app_cycle_counter: AppCycleCounter,
     app_input_handler: AppInputHandler,
     app_world: AppWorld,
+
+    last_time: Instant,
+    delta_time: f64,
+    ups: u64,
 }
 
 impl App {
@@ -52,9 +51,11 @@ impl App {
     pub async fn from_app_config(app_config: AppConfig) -> Self {
         Self {
             app_config,
-            app_cycle_counter: AppCycleCounter::default(),
             app_input_handler: AppInputHandler::new(),
             app_world: AppWorld::new(),
+            last_time: Instant::now(),
+            delta_time: 0.0,
+            ups: 0,
         }
     }
 
@@ -87,13 +88,11 @@ impl App {
         let engine = Arc::new(Engine::initialize(window.clone()).await);
         engine.configure_surface();
 
-        // << Cycle Calculation >>
-        self.app_cycle_counter.reset();
+        let engine_backend = engine.get_adapter().get_info().backend.to_str();
+        log::info!("Engine Backend: {engine_backend}");
 
+        // World
         self.spawn_world().await;
-
-        // TODO: FPS Limit cycle calculation
-        // I.e. Update every cycle, but render only after delta time is past
 
         // App Context
         let mut app_context = AppContext::default();
@@ -106,23 +105,15 @@ impl App {
         app_context.clear_colour_index = 0;
         app_context.clear_colour_increasing = true;
 
-        
+        // << Cycle Calculation >>
+        self.last_time = Instant::now();
+        self.delta_time = 0.0;
+        self.ups = 0;
 
         event_loop.run(move |event, _target, control_flow| {
             // Immediately start a new cycle once a loop is completed.
             // Ideal for games, but more resource intensive.
             *control_flow = ControlFlow::Poll;
-
-            if let Some((delta_time, ups)) = self.app_cycle_counter.tick() {
-                // Update Window Title
-                window.get_window().set_title(&format!(
-                    "WGPU @ {} - UPS: {}/s (Δ {}s)",
-                    engine.get_adapter().get_info().backend.to_str(), ups, delta_time
-                ));
-
-                // Update performance outputs
-                log::debug!("UPS: {}/s (delta time: {}s)", ups, delta_time);
-            }
 
             // << Events >>
             match event {
@@ -161,20 +152,75 @@ impl App {
                 Event::RedrawEventsCleared => {
                     // Request to redraw the next cycle
                     window.get_window().request_redraw();
-                }
+                },
+                Event::MainEventsCleared => self.handle_main_events_cleared(&engine_backend, &window, control_flow),
                 _ => (),
             }
-            
-            // Updates!
-            if let Some(r_control_flow) = &self.handle_update() {
-                if *control_flow != *r_control_flow {
-                    log::warn!("Switching control flow from {:?} to {:?}", *control_flow, r_control_flow);
-                    *control_flow = *r_control_flow;
-                }
+        });
+    }
+
+    /// Handles the main events cleared event.
+    /// This event is triggered after **all** other events have been processed.
+    /// This event **should** be relatively consistent in comparison.
+    ///
+    /// This function is used to update our app, including:
+    /// - Updateables
+    /// - UPS & Delta calculation
+    /// - Performance outputs
+    fn handle_main_events_cleared(
+        &mut self,
+        backend_name: &str,
+        window: &AppWindow,
+        control_flow: &mut ControlFlow,
+    ) {
+        // Take now time
+        let now = Instant::now();
+        // Get the duration of elapsed time since last update
+        let elapsed = self.last_time.elapsed();
+        // Update last time with now time
+        self.last_time = now;
+
+        // Add the elapsed time to the delta time
+        self.delta_time += elapsed.as_secs_f64();
+        // Increment UPS cycle count
+        self.ups += 1;
+
+        // Call updateables
+        self.app_world.call_updateables_on_cycle(self.delta_time);
+
+        // If a second has past, call updates
+        if self.delta_time >= 1.0 {
+            #[cfg(debug_assertions)]
+            {
+                // Update performance outputs
+                log::debug!("UPS: {}/s (delta time: {}s)", self.ups, self.delta_time);
+
+                // Update Window Title
+                window.get_window().set_title(&format!(
+                    "WGPU @ {} - UPS: {}/s (Δ {}s)",
+                    backend_name, self.ups, self.delta_time
+                ));
             }
 
-            window.get_window().request_redraw();
-        });
+            // Check for main keyboard inputs
+            if self
+                .app_input_handler
+                .are_all_keys_pressed(&vec![VirtualKeyCode::LAlt, VirtualKeyCode::F4])
+                || self
+                    .app_input_handler
+                    .is_key_pressed(&VirtualKeyCode::Escape)
+            {
+                log::warn!("Exit condition reached!");
+                *control_flow = ControlFlow::Exit;
+            }
+
+            // Call updateables
+            self.app_world.call_updateables_on_second(self.delta_time);
+
+            // Reset counters
+            self.ups = 0;
+            self.delta_time -= 1.0;
+        }
     }
 
     fn handle_keyboard_input(
@@ -191,13 +237,18 @@ impl App {
     }
 
     fn handle_resize(&mut self, new_size: PhysicalSize<u32>, engine: &Engine) {
-        log::debug!(
+        log::info!(
             "Resize detected! Changing from {}x{} to {}x{}",
             self.app_config.window_config.size.0,
             self.app_config.window_config.size.1,
             &new_size.width,
             &new_size.height
         );
+
+        if new_size.width <= 0 || new_size.height <= 0 {
+            log::error!("Invalid new window size received!");
+            return;
+        }
 
         if !engine.get_device().poll(wgpu::MaintainBase::Wait) {
             log::error!("Failed to poll device before resizing!");
@@ -216,42 +267,6 @@ impl App {
         engine.configure_surface();
     }
 
-    pub fn get_app_config(&self) -> &AppConfig {
-        &self.app_config
-    }
-
-    fn handle_update(&mut self) -> Option<ControlFlow> {
-        // Exit condition
-        if self
-            .app_input_handler
-            .are_all_keys_pressed(&vec![VirtualKeyCode::LAlt, VirtualKeyCode::F4])
-            || self
-                .app_input_handler
-                .is_key_pressed(&VirtualKeyCode::Escape)
-        {
-            log::warn!("Exit condition reached!");
-            return Some(ControlFlow::Exit);
-        }
-
-        // World Spawn
-        if self
-            .app_input_handler
-            .is_key_pressed(&VirtualKeyCode::Space)
-        {
-            let object = ClearScreenObject::new();
-            let object_boxed = Box::new(object);
-            self.app_world.spawn_object(object_boxed);
-
-            log::debug!("Object: {}", self.app_world.count_object());
-            log::debug!("Updateable: {}", self.app_world.count_updateable());
-            log::debug!("Renderable: {}", self.app_world.count_renderable());
-        }
-
-        self.app_world.call_updateables();
-
-        None
-    }
-
     fn handle_redraw(&mut self, engine: Arc<Engine>) {
         let output_surface_texture = engine
             .get_surface()
@@ -263,10 +278,16 @@ impl App {
             .create_view(&TextureViewDescriptor::default());
 
         // Build command buffers for frame
-        let command_buffers = self.app_world.call_renderables(engine.clone(), &output_surface_texture_view);
+        let command_buffers = self
+            .app_world
+            .call_renderables(engine.clone(), &output_surface_texture_view);
 
         // Submit command buffer
         engine.get_queue().submit(command_buffers);
         output_surface_texture.present();
+    }
+
+    pub fn get_app_config(&self) -> &AppConfig {
+        &self.app_config
     }
 }
