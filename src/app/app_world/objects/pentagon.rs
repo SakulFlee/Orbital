bbb4bb0c74670e5afcc4df05ece4a248e646089d
Lukas Weber <dev@sakul-flee.de>
2023-09-10 00:42:43 +0200

    Texture (almost)

--- src/app/app_world/objects/pentagon.rs
+++ src/app/app_world/objects/pentagon.rs
@@ -7,27 +7,27 @@ impl Pentagon {
         // A
         Vertex {
             position: [0.0, 0.5, 0.0],
-            color: [1.0, 0.0, 0.0],
+            tex_coords: [0.5, 0.5],
         },
         // B
         Vertex {
             position: [-0.5, 0.15, 0.0],
-            color: [0.5, 0.5, 0.0],
+            tex_coords: [0.0, 1.0],
         },
         // C
         Vertex {
             position: [-0.30, -0.5, 0.0],
-            color: [0.0, 0.5, 0.0],
+            tex_coords: [0.5, 0.0],
         },
         // D
         Vertex {
             position: [0.30, -0.5, 0.0],
-            color: [0.0, 0.0, 0.5],
+            tex_coords: [0.0, 0.5],
         },
         // E
         Vertex {
             position: [0.5, 0.15, 0.0],
-            color: [0.5, 0.0, 0.5],
+            tex_coords: [1.0, 0.0],
         },
     ];
 
