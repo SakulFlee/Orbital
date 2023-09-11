897f9d7de135197b1ad6d0d1bd70a84e8ab1df0a
Lukas Weber <dev@sakul-flee.de>
2023-09-11 18:19:27 +0200

    Removed unused AppContext

--- src/app/app_context.rs
+++
@@ -1,8 +0,0 @@
-use wgpu::Color;
-
-#[derive(Debug, Clone, Default)]
-pub struct AppContext {
-    pub clear_colour: Color,
-    pub clear_colour_index: u32,
-    pub clear_colour_increasing: bool,
-}
