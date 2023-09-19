d33e940f61baf861dce54e606cbbd77be05183d3
Lukas Weber <dev@sakul-flee.de>
2023-09-19 20:57:01 +0200

    Dictionary

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
-use wgpu_engine::{log_init, App};
+use wgpu_engine::{log_init, WGPURenderingEngine};
+use winit::{event_loop::EventLoop, window::WindowBuilder};
 
-// TODO: Remove Tokio
+#[cfg(test)]
+mod tests;
 
-/// Main function
-#[tokio::main(worker_threads = 16)]
-async fn main() {
+fn main() {
     // Log initialization
     log_init();
 
     // App
-    let app = App::from_app_config_default_path();
-    app.hijack_thread_and_run().await;
+    // let app = App::from_app_config_default_path();
+    // app.hijack_thread_and_run().await;
+
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .build(&event_loop)
+        .expect("Window creation failed");
+
+    let engine = WGPURenderingEngine::new(&window).expect("Engine creation failed");
 }
