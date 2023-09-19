8aa0e898159eda3ee0ef39e7b836d602cec46796
Lukas Weber <dev@sakul-flee.de>
2023-09-19 02:35:37 +0200

    Split into lib

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+pub mod app;
+pub use app::*;
+
+pub mod engine;
+pub use engine::*;
+
+pub mod log;
+pub use log::*;
+
+pub mod camera;
+pub use camera::*;
+
+pub const APP_NAME: &'static str = "WGPU-Engine";
