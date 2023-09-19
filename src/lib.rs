42541ee08567c3bf4c7a83ed3a9cbddad8f3b4ab
Lukas Weber <dev@sakul-flee.de>
2023-09-19 20:56:35 +0200

    Added WGPUEngine implementations

--- src/lib.rs
+++ src/lib.rs
@@ -16,4 +16,22 @@ pub use engine_error::*;
 pub mod engine_result;
 pub use engine_result::*;
 
+pub mod surface_helper;
+pub use surface_helper::*;
+
+pub mod surface_configuration_helper;
+pub use surface_configuration_helper::*;
+
+pub mod computing_engine;
+pub use computing_engine::*;
+
+pub mod rendering_engine;
+pub use rendering_engine::*;
+
+pub mod wgpu_computing_engine;
+pub use wgpu_computing_engine::*;
+
+pub mod wgpu_rendering_engine;
+pub use wgpu_rendering_engine::*;
+
 pub const APP_NAME: &'static str = "WGPU-Engine";
