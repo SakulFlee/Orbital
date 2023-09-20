ce619560adddecc8d678055a7f8f249a8500fc50
Lukas Weber <dev@sakul-flee.de>
2023-09-20 19:39:57 +0200

    Material, Mesh & Vertex

---
+++ src/engine/resource/mesh/t_mesh.rs
@@ -0,0 +1,14 @@
+use std::ops::Range;
+
+use wgpu::Buffer;
+
+use crate::engine::TMaterial;
+
+pub trait TMesh {
+    fn get_vertex_buffer(&self) -> &Buffer;
+    fn get_index_buffer(&self) -> &Buffer;
+    fn get_index_count(&self) -> u32;
+    fn get_instance_range(&self) -> Range<u32>;
+    fn set_instance_range(&mut self, range: Range<u32>);
+    fn get_material(&self) -> Option<&Box<dyn TMaterial>>;
+}
