12672b3ffc41f2eea96f2e7554b223e59d9fc240
Lukas Weber <dev@sakul-flee.de>
2023-09-24 15:42:33 +0200

    Added todo

--- src/engine/resource/material/mod.rs
+++ src/engine/resource/material/mod.rs
@@ -4,3 +4,7 @@ pub use t_material::*;
 mod standard_material;
 pub use standard_material::*;
 
+// TODO: Uniform colour material:
+// - Single RGB value for the whole material
+// - Needs to be understood by shader
+// Needed?
