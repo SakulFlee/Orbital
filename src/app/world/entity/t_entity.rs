2fd63f7292347c9a75d872ff86772e44001864bc
Lukas Weber <dev@sakul-flee.de>
2023-09-26 04:04:49 +0200

    Added ability to return multiple EntityActions

--- src/app/world/entity/t_entity.rs
+++ src/app/world/entity/t_entity.rs
@@ -10,9 +10,8 @@ use super::{EntityAction, EntityConfiguration};
 pub trait TEntity {
     fn get_entity_configuration(&self) -> EntityConfiguration;
 
-    // TODO: Vec<EntityAction>
-    fn update(&mut self, _delta_time: f64, _input_handler: &InputHandler) -> EntityAction {
-        EntityAction::Keep
+    fn update(&mut self, _delta_time: f64, _input_handler: &InputHandler) -> Vec<EntityAction> {
+        vec![EntityAction::Keep]
     }
 
     fn prepare_render(&mut self, _device: &Device, _queue: &Queue) -> EngineResult<()> {
