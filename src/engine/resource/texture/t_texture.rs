7b9c900c16f00f84a93fe839789cbc2451bfdac6
Lukas Weber <dev@sakul-flee.de>
2023-09-28 01:55:59 +0200

    RFC#344 compliance

--- src/engine/resource/texture/t_texture.rs
+++ src/engine/resource/texture/t_texture.rs
@@ -1,7 +1,7 @@
 use wgpu::{Sampler, Texture, TextureView};
 
 pub trait TTexture {
-    fn get_texture(&self) -> &Texture;
-    fn get_view(&self) -> &TextureView;
-    fn get_sampler(&self) -> &Sampler;
+    fn texture(&self) -> &Texture;
+    fn view(&self) -> &TextureView;
+    fn sampler(&self) -> &Sampler;
 }
