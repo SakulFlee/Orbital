a2fa95c1e494c1796e0a4dc1fdf030d001b9f861
Lukas Weber <dev@sakul-flee.de>
2023-09-28 01:55:52 +0200

    RFC#344 compliance

--- src/engine/resource/material/t_material.rs
+++ src/engine/resource/material/t_material.rs
@@ -5,16 +5,16 @@ use wgpu::{BindGroup, BindGroupLayout};
 use crate::engine::{DiffuseTexture, LogicalDevice};
 
 pub trait TMaterial {
-    fn get_name(&self) -> &str;
-    fn get_diffuse_texture(&self) -> &DiffuseTexture;
-    fn get_bind_group_layout(logical_device: &LogicalDevice) -> BindGroupLayout
+    fn name(&self) -> &str;
+    fn diffuse_texture(&self) -> &DiffuseTexture;
+    fn bind_group_layout(logical_device: &LogicalDevice) -> BindGroupLayout
     where
         Self: Sized;
-    fn get_bind_group(&self) -> &BindGroup;
+    fn bind_group(&self) -> &BindGroup;
 }
 
 impl Debug for dyn TMaterial {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-        f.write_fmt(format_args!("TMaterial: {}", self.get_name()))
+        f.write_fmt(format_args!("TMaterial: {}", self.name()))
     }
 }
