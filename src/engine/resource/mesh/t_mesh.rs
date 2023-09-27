b703ffb0b168d0cfc2a168effd20d28bedd7560d
Lukas Weber <dev@sakul-flee.de>
2023-09-28 01:55:54 +0200

    RFC#344 compliance

--- src/engine/resource/mesh/t_mesh.rs
+++ src/engine/resource/mesh/t_mesh.rs
@@ -3,12 +3,12 @@ use wgpu::Buffer;
 use crate::engine::{StandardInstance, TMaterial};
 
 pub trait TMesh {
-    fn get_vertex_buffer(&self) -> &Buffer;
-    fn get_index_buffer(&self) -> &Buffer;
-    fn get_index_count(&self) -> u32;
-    fn get_instances(&mut self) -> &mut Vec<StandardInstance>;
-    fn get_instance_count(&self) -> u32;
-    fn get_instance_buffer(&self) -> &Buffer;
-    fn get_material(&self) -> &dyn TMaterial;
-    fn get_name(&self) -> Option<String>;
+    fn vertex_buffer(&self) -> &Buffer;
+    fn index_buffer(&self) -> &Buffer;
+    fn index_count(&self) -> u32;
+    fn instances(&mut self) -> &mut Vec<StandardInstance>;
+    fn instance_count(&self) -> u32;
+    fn instance_buffer(&self) -> &Buffer;
+    fn material(&self) -> &dyn TMaterial;
+    fn name(&self) -> Option<String>;
 }
