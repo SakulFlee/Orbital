99278c34717c9b2afa51165651fbd6e91b72d826
Lukas Weber <dev@sakul-flee.de>
2023-10-03 03:16:07 +0200

    Mouse input handler

--- src/app/input/mod.rs
+++ src/app/input/mod.rs
@@ -1,25 +1,31 @@
 use winit::event::VirtualKeyCode;
 
-use self::keyboard_input_handler::KeyboardInputHandler;
+use self::{keyboard_input_handler::KeyboardInputHandler, mouse_input_handler::MouseInputHandler};
 
 pub mod controller_input_handler;
 pub mod keyboard_input_handler;
 pub mod mouse_input_handler;
 
-#[derive(Debug, Default)]
+#[derive(Debug)]
 pub struct InputHandler {
     keyboard_input_handler: KeyboardInputHandler,
+    mouse_input_handler: MouseInputHandler,
 }
 
 impl InputHandler {
     pub fn new() -> Self {
         Self {
             keyboard_input_handler: KeyboardInputHandler::new(),
+            mouse_input_handler: MouseInputHandler::new(),
         }
     }
 
     pub fn keyboard_input_handler(&mut self) -> &mut KeyboardInputHandler {
         &mut self.keyboard_input_handler
+    }
+
+    pub fn mouse_input_handler(&mut self) -> &mut MouseInputHandler {
+        &mut self.mouse_input_handler
     }
 
     pub fn is_key_pressed(&self, pressed_key: &VirtualKeyCode) -> bool {
