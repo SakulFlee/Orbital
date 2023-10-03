b68016f9d38d4412e809bffbaf7369dacc3a960a
Lukas Weber <dev@sakul-flee.de>
2023-10-03 03:57:39 +0200

    Hotfix

--- src/app/input/mod.rs
+++ src/app/input/mod.rs
@@ -1,4 +1,4 @@
-use winit::event::VirtualKeyCode;
+use winit::event::{MouseScrollDelta, TouchPhase, VirtualKeyCode};
 
 use self::{keyboard_input_handler::KeyboardInputHandler, mouse_input_handler::MouseInputHandler};
 
@@ -20,11 +20,19 @@ impl InputHandler {
         }
     }
 
-    pub fn keyboard_input_handler(&mut self) -> &mut KeyboardInputHandler {
+    pub fn keyboard_input_handler(&self) -> &KeyboardInputHandler {
+        &self.keyboard_input_handler
+    }
+
+    pub fn keyboard_input_handler_mut(&mut self) -> &mut KeyboardInputHandler {
         &mut self.keyboard_input_handler
     }
 
-    pub fn mouse_input_handler(&mut self) -> &mut MouseInputHandler {
+    pub fn mouse_input_handler(&mut self) -> &MouseInputHandler {
+        &self.mouse_input_handler
+    }
+
+    pub fn mouse_input_handler_mut(&mut self) -> &mut MouseInputHandler {
         &mut self.mouse_input_handler
     }
 
@@ -38,6 +46,30 @@ impl InputHandler {
 
     pub fn is_any_key_pressed(&self, pressed_keys: &[VirtualKeyCode]) -> bool {
         self.keyboard_input_handler.is_any_pressed(pressed_keys)
+    }
+
+    pub fn cursor_position(&self) -> (f64, f64) {
+        self.mouse_input_handler.cursor_position()
+    }
+
+    pub fn is_inside(&self) -> bool {
+        self.mouse_input_handler.is_inside()
+    }
+
+    pub fn lmb_pressed(&self) -> bool {
+        self.mouse_input_handler.lmb_pressed()
+    }
+
+    pub fn rmb_pressed(&self) -> bool {
+        self.mouse_input_handler.rmb_pressed()
+    }
+
+    pub fn mmb_pressed(&self) -> bool {
+        self.mouse_input_handler.mmb_pressed()
+    }
+
+    pub fn scroll(&self) -> Option<(TouchPhase, MouseScrollDelta)> {
+        self.mouse_input_handler.scroll()
     }
 }
 
