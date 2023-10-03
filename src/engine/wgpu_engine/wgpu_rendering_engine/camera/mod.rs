56ee6b6e149ea4666dc7da29ca5f320f0f41feeb
Lukas Weber <dev@sakul-flee.de>
2023-10-03 02:52:59 +0200

    New Camera

--- src/engine/wgpu_engine/wgpu_rendering_engine/camera/mod.rs
+++ src/engine/wgpu_engine/wgpu_rendering_engine/camera/mod.rs
@@ -1,4 +1,6 @@
-use cgmath::{perspective, Deg, Matrix4, Point3, SquareMatrix, Vector3};
+use std::f32::consts::FRAC_PI_2;
+
+use cgmath::{perspective, Deg, InnerSpace, Matrix4, Point3, Rad, SquareMatrix, Vector3};
 use wgpu::{
     util::{BufferInitDescriptor, DeviceExt},
     BindGroup, BindGroupDescriptor, BindGroupEntry, BindGroupLayout, BindGroupLayoutDescriptor,
@@ -13,17 +15,11 @@ pub use camera_change::*;
 
 use crate::engine::LogicalDevice;
 
+#[derive(Debug)]
 pub struct Camera {
-    eye: Point3<f32>,
-    target: Point3<f32>,
-    up: Vector3<f32>,
-    aspect: f32,
-    fovy: f32,
-    znear: f32,
-    zfar: f32,
-    view_projection: Matrix4<f32>,
-    buffer: Buffer,
-    bind_group: BindGroup,
+    position: Point3<f32>,
+    yaw: Rad<f32>,
+    pitch: Rad<f32>,
 }
 
 impl Camera {
@@ -37,216 +33,64 @@ impl Camera {
 
     pub const DEFAULT_CAMERA_EYE_POSITION: (f32, f32, f32) = (0.0, 1.0, 2.0);
 
-    pub const BIND_GROUP_LAYOUT_DESCRIPTOR: BindGroupLayoutDescriptor<'static> =
-        BindGroupLayoutDescriptor {
-            label: Some("Camera Bind Group Layout"),
-            entries: &[BindGroupLayoutEntry {
-                binding: 0,
-                visibility: ShaderStages::VERTEX_FRAGMENT,
-                ty: BindingType::Buffer {
-                    ty: BufferBindingType::Uniform,
-                    has_dynamic_offset: false,
-                    min_binding_size: None,
-                },
-                count: None,
-            }],
-        };
+    const SAFE_FRAC_PI_2: f32 = FRAC_PI_2 - 0.0001;
 
-    pub fn from_window_size(logical_device: &LogicalDevice, window_size: (u32, u32)) -> Self {
-        Self::new(
-            logical_device,
-            Self::DEFAULT_CAMERA_EYE_POSITION.into(),
-            (0.0, 0.0, 0.0).into(),
+    pub fn new<V: Into<Point3<f32>>, Y: Into<Rad<f32>>, P: Into<Rad<f32>>>(
+        position: V,
+        yaw: Y,
+        pitch: P,
+    ) -> Self {
+        Self {
+            position: position.into(),
+            yaw: yaw.into(),
+            pitch: pitch.into(),
+        }
+    }
+
+    pub fn calculate_matrix(&self) -> Matrix4<f32> {
+        let (sin_pitch, cos_pitch) = self.pitch.0.sin_cos();
+        let (sin_yaw, cos_yaw) = self.yaw.0.sin_cos();
+
+        Matrix4::look_to_rh(
+            self.position,
+            Vector3 {
+                x: cos_pitch * cos_yaw,
+                y: sin_pitch,
+                z: cos_pitch * sin_yaw,
+            }
+            .normalize(),
             Vector3::unit_y(),
-            window_size.0 as f32 / window_size.1 as f32,
-            45.0,
-            0.1,
-            100.0,
         )
     }
+}
 
-    #[allow(clippy::too_many_arguments)] // TODO: Check clippy again after Logical/Physical Device split
-    pub fn new(
-        logical_device: &LogicalDevice,
-        eye: Point3<f32>,
-        target: Point3<f32>,
-        up: Vector3<f32>,
-        aspect: f32,
-        fovy: f32,
-        znear: f32,
-        zfar: f32,
-    ) -> Self {
-        let empty_uniform = UCamera::empty();
-        let buffer = logical_device
-            .device()
-            .create_buffer_init(&BufferInitDescriptor {
-                label: Some("Camera Buffer"),
-                contents: bytemuck::cast_slice(&[empty_uniform]),
-                usage: BufferUsages::UNIFORM | BufferUsages::COPY_DST,
-            });
+// TODO: Move out
+pub struct Projection {
+    aspect: f32,
+    fovy: Rad<f32>,
+    znear: f32,
+    zfar: f32,
+}
 
-        let bind_group_layout = Self::bind_group_layout(logical_device);
-
-        let bind_group = logical_device
-            .device()
-            .create_bind_group(&BindGroupDescriptor {
-                label: Some("Camera Bind Group"),
-                layout: &bind_group_layout,
-                entries: &[BindGroupEntry {
-                    binding: 0,
-                    resource: buffer.as_entire_binding(),
-                }],
-            });
-
-        let mut camera = Self {
-            eye,
-            target,
-            up,
-            aspect,
-            fovy,
+impl Projection {
+    pub fn new<F: Into<Rad<f32>>>(width: u32, height: u32, fovy: F, znear: f32, zfar: f32) -> Self {
+        Self {
+            aspect: width as f32 / height as f32,
+            fovy: fovy.into(),
             znear,
             zfar,
-            view_projection: Matrix4::identity(),
-            buffer,
-            bind_group,
-        };
-
-        camera.update_buffer(logical_device);
-
-        camera
-    }
-
-    fn update_view_projection_matrix(&mut self) {
-        let view_matrix = Matrix4::look_at_rh(self.eye, self.target, self.up);
-        let projection_matrix = perspective(Deg(self.fovy), self.aspect, self.znear, self.zfar);
-
-        self.view_projection = Camera::OPENGL_TO_WGPU_MATRIX * projection_matrix * view_matrix;
-    }
-
-    pub fn update_buffer(&mut self, logical_device: &LogicalDevice) {
-        // Make sure the view matrix is up-to-date
-        self.update_view_projection_matrix();
-
-        // Transform into uniform
-        let uniform = self.to_uniform();
-
-        // Write uniform into buffer
-        logical_device
-            .queue()
-            .write_buffer(&self.buffer, 0, bytemuck::cast_slice(&[uniform]))
-    }
-
-    pub fn to_uniform(&self) -> UCamera {
-        UCamera::from_camera(self)
-    }
-
-    pub fn apply_camera_change(
-        &mut self,
-        logical_device: &LogicalDevice,
-        camera_change: CameraChange,
-    ) {
-        if let Some(eye) = camera_change.eye() {
-            self.set_eye(eye);
         }
-
-        if let Some(target) = camera_change.target() {
-            self.set_target(target);
-        }
-
-        if let Some(up) = camera_change.up() {
-            self.set_up(up);
-        }
-
-        if let Some(aspect) = camera_change.aspect() {
-            self.set_aspect(aspect);
-        }
-
-        if let Some(fovy) = camera_change.fovy() {
-            self.set_fovy(fovy);
-        }
-
-        if let Some(znear) = camera_change.znear() {
-            self.set_znear(znear);
-        }
-
-        if let Some(zfar) = camera_change.zfar() {
-            self.set_zfar(zfar);
-        }
-
-        self.update_buffer(logical_device);
     }
 
-    pub fn eye(&self) -> Point3<f32> {
-        self.eye
+    pub fn resize(&mut self, width: u32, height: u32) {
+        self.aspect = width as f32 / height as f32
     }
 
-    pub fn set_eye(&mut self, eye: Point3<f32>) {
-        self.eye = eye;
-    }
-
-    pub fn target(&self) -> Point3<f32> {
-        self.target
-    }
-
-    pub fn set_target(&mut self, target: Point3<f32>) {
-        self.target = target;
-    }
-
-    pub fn up(&self) -> Vector3<f32> {
-        self.up
-    }
-
-    pub fn set_up(&mut self, up: Vector3<f32>) {
-        self.up = up;
-    }
-
-    pub fn aspect(&self) -> f32 {
-        self.aspect
-    }
-
-    pub fn set_aspect(&mut self, aspect: f32) {
-        self.aspect = aspect;
-    }
-
-    pub fn fovy(&self) -> f32 {
-        self.fovy
-    }
-
-    pub fn set_fovy(&mut self, fovy: f32) {
-        self.fovy = fovy;
-    }
-
-    pub fn znear(&self) -> f32 {
-        self.znear
-    }
-
-    pub fn set_znear(&mut self, znear: f32) {
-        self.znear = znear;
-    }
-
-    pub fn zfar(&self) -> f32 {
-        self.zfar
-    }
-
-    pub fn set_zfar(&mut self, zfar: f32) {
-        self.zfar = zfar;
-    }
-
-    pub fn view_projection(&self) -> Matrix4<f32> {
-        self.view_projection
-    }
-
-    pub fn buffer(&self) -> &Buffer {
-        &self.buffer
-    }
-
-    pub fn bind_group_layout(logical_device: &LogicalDevice) -> BindGroupLayout {
-        logical_device
-            .device()
-            .create_bind_group_layout(&Self::BIND_GROUP_LAYOUT_DESCRIPTOR)
-    }
-
-    pub fn bind_group(&self) -> &BindGroup {
-        &self.bind_group
+    pub fn calculate_matrix(&self) -> Matrix4<f32> {
+        Camera::OPENGL_TO_WGPU_MATRIX * perspective(self.fovy, self.aspect, self.znear, self.zfar)
     }
 }
+
+// TODO: Mouse Handler
+// TODO: Actually update Camera
+// TODO: Uniform changes
