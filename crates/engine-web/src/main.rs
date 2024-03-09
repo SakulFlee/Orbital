4fba26e4071f1069d65849f2bbf3a2196ab22be8
SakulFlee <dev@sakul-flee.de>
2024-03-09 02:10:53 +0100

    First working Tauri

---
+++ crates/engine-web/src/main.rs
@@ -0,0 +1,9 @@
+use sycamore::prelude::*;
+
+fn main() {
+    sycamore::render(|| {
+        view! {
+            p { "Hello, World!" }
+        }
+    });
+}
--- crates/engine/src/app.rs
+++ crates/engine/src/app.rs
@@ -1,59 +1,84 @@
-use winit::{
-    event::{Event, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
-    window::Window,
-};
+#![allow(unsafe_code)]
 
-pub struct App {
-    event_loop: EventLoop<()>,
-    window: Window,
+use serde::{Deserialize, Serialize};
+use serde_wasm_bindgen::to_value;
+use sycamore::futures::spawn_local_scoped;
+use sycamore::prelude::*;
+use sycamore::rt::Event;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "core"])]
+    async fn invoke(cmd: &str, args: JsValue) -> JsValue;
+
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
 }
 
-impl App {
-    pub fn new(control_flow: ControlFlow) -> Self {
-        let event_loop = EventLoop::new().unwrap();
-        event_loop.set_control_flow(control_flow);
+#[derive(Serialize, Deserialize)]
+struct GreetArgs<'a> {
+    name: &'a str,
+}
 
-        let window = Window::new(&event_loop).unwrap();
+#[component]
+pub fn app<G: Html>(cx: Scope) -> View<G> {
+    let name = create_signal(cx, String::new());
+    let greet_msg = create_signal(cx, String::new());
 
-        Self { event_loop, window }
-    }
+    let greet = move |e: Event| {
+        e.prevent_default();
+        spawn_local_scoped(cx, async move {
+            unsafe {
+                // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+                let new_msg =
+                    invoke("greet", to_value(&GreetArgs { name: &name.get() }).unwrap()).await;
 
-    pub fn run(self) {
-        self.event_loop
-            .run(move |event, elwt| {
-                match event {
-                    Event::WindowEvent {
-                        event: WindowEvent::CloseRequested,
-                        ..
-                    } => {
-                        println!("The close button was pressed; stopping");
-                        elwt.exit();
-                    }
-                    Event::AboutToWait => {
-                        // Application update code.
+                log(&new_msg.as_string().unwrap());
+            }
 
-                        // Queue a RedrawRequested event.
-                        //
-                        // You only need to call this if you've determined that you need to redraw in
-                        // applications which do not always need to. Applications that redraw continuously
-                        // can render here instead.
-                        self.window.request_redraw();
-                    }
-                    Event::WindowEvent {
-                        event: WindowEvent::RedrawRequested,
-                        ..
-                    } => {
-                        // Redraw the application.
-                        //
-                        // It's preferable for applications that do not render continuously to render in
-                        // this event rather than in AboutToWait, since rendering in here allows
-                        // the program to gracefully handle redraws requested by the OS.
-                        println!("Redraw!");
-                    }
-                    _ => (),
+            greet_msg.set(new_msg.as_string().unwrap());
+        })
+    };
+
+    view! { cx,
+        main(class="container") {
+            div(class="row") {
+                a(href="https://tauri.app",target="_blank") {
+                    img(src="public/tauri.svg",class="logo tauri",alt="Tauri logo")
                 }
-            })
-            .unwrap();
+                a(href="https://sycamore-rs.netlify.app",target="_blank") {
+                    img(src="public/sycamore.svg",class="logo sycamore",alt="Sycamore logo")
+                }
+            }
+            p {
+                "Click on the Tauri and Sycamore logos to learn more."
+            }
+            p {
+                "Recommended IDE setup: "
+                a(href="https://code.visualstudio.com/",target="_blank") {
+                    "VS Code"
+                }
+                " + "
+                a(href="https://github.com/tauri-apps/tauri-vscode",target="_blank") {
+                    "Tauri"
+                }
+                " + "
+                a(href="https://github.com/rust-lang/rust-analyzer",target="_blank") {
+                    "rust-analyzer"
+                }
+            }
+            form(class="row",on:submit=greet) {
+                input(id="greet-input",bind:value=name,placeholder="Enter a name...")
+                button(type="submit") {
+                    "Greet"
+                }
+            }
+            p {
+                b {
+                    (greet_msg.get())
+                }
+            }
+        }
     }
 }
