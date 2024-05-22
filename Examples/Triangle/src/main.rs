98e49666a26641850ae45ccb18dde507f038478c
SakulFlee <dev@sakul-flee.de>
2024-05-22 19:02:55 +0200

    Winit eventloop builder changes

--- Examples/Triangle/src/main.rs
+++ Examples/Triangle/src/main.rs
@@ -1,8 +1,8 @@
-use akimo_runtime::winit::event_loop::EventLoopBuilder;
+use akimo_runtime::winit::event_loop::EventLoop;
 use app_example_triangle::entrypoint::entrypoint;
 
 fn main() {
-    let event_loop = EventLoopBuilder::new().build();
+    let event_loop = EventLoop::builder().build();
 
     entrypoint(event_loop);
 }
