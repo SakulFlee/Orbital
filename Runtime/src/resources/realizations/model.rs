9f53029663802cce2b08bb79be4c9a605b08c4cd
SakulFlee <dev@sakul-flee.de>
2024-06-09 22:59:26 +0200

    Instancing changes

--- Runtime/src/resources/realizations/model.rs
+++ Runtime/src/resources/realizations/model.rs
@@ -1,15 +1,22 @@
-use wgpu::{Device, Queue, TextureFormat};
+use wgpu::{
+    util::{BufferInitDescriptor, DeviceExt},
+    Buffer, BufferUsages, Device, Queue, TextureFormat,
+};
 
 use crate::{
     error::Error,
-    resources::{ImportDescriptor, MaterialDescriptor, MeshDescriptor, ModelDescriptor},
+    resources::{
+        ImportDescriptor, Instancing, MaterialDescriptor, MeshDescriptor, ModelDescriptor,
+    },
 };
 
-use super::{Material, Mesh};
+use super::{instance::Instance, Material, Mesh};
 
 pub struct Model {
     mesh: Mesh,
     material: Material,
+    instances: Vec<Instance>,
+    instance_buffer: Buffer,
 }
 
 impl Model {
@@ -20,32 +27,38 @@ impl Model {
         queue: &Queue,
     ) -> Result<Self, Error> {
         match descriptor {
-            ModelDescriptor::FromDescriptors(mesh_descriptor, material_descriptor) => {
+            ModelDescriptor::FromDescriptors(mesh_descriptor, material_descriptor, instancing) => {
                 Self::from_descriptors(
                     mesh_descriptor,
                     material_descriptor,
+                    instancing,
                     surface_format,
                     device,
                     queue,
                 )
             }
             #[cfg(feature = "gltf")]
-            ModelDescriptor::FromGLTF(file, scene_import_descriptor, model_import_descriptor) => {
-                Self::from_gltf(
-                    file,
-                    scene_import_descriptor,
-                    model_import_descriptor,
-                    surface_format,
-                    device,
-                    queue,
-                )
-            }
+            ModelDescriptor::FromGLTF(
+                file,
+                scene_import_descriptor,
+                model_import_descriptor,
+                instancing,
+            ) => Self::from_gltf(
+                file,
+                scene_import_descriptor,
+                model_import_descriptor,
+                instancing,
+                surface_format,
+                device,
+                queue,
+            ),
         }
     }
 
     pub fn from_descriptors(
         mesh_descriptor: &MeshDescriptor,
         material_descriptor: &MaterialDescriptor,
+        instancing: &Instancing,
         surface_format: &TextureFormat,
         device: &Device,
         queue: &Queue,
@@ -55,7 +68,11 @@ impl Model {
         let material =
             Material::from_descriptor(material_descriptor, surface_format, device, queue)?;
 
-        Ok(Self::from_existing(mesh, material))
+        let instances = Self::convert_instancing(instancing);
+
+        Ok(Self::from_existing(
+            mesh, material, instances, device, queue,
+        ))
     }
 
     #[cfg(feature = "gltf")]
@@ -63,6 +80,7 @@ impl Model {
         file: &'static str,
         scene_import_descriptor: &ImportDescriptor,
         model_import_descriptor: &ImportDescriptor,
+        instancing: &Instancing,
         surface_format: &TextureFormat,
         device: &Device,
         queue: &Queue,
@@ -97,17 +115,81 @@ impl Model {
             return Err(Error::ModelNotFound);
         };
 
+        let instances = Self::convert_instancing(instancing);
+
         // Realize model
         let model = Self::from_existing(
             Mesh::from_gltf(&model, device)?,
             Material::from_gltf(&model.material(), surface_format, device, queue)?,
+            instances,
+            device,
+            queue,
         );
 
         Ok(model)
     }
 
-    pub fn from_existing(mesh: Mesh, material: Material) -> Self {
-        Self { mesh, material }
+    pub fn from_existing(
+        mesh: Mesh,
+        material: Material,
+        instances: Vec<Instance>,
+        device: &Device,
+        queue: &Queue,
+    ) -> Self {
+        let instance_buffer = Self::make_instance_buffer(&instances, device, queue);
+
+        Self {
+            mesh,
+            material,
+            instances,
+            instance_buffer,
+        }
+    }
+
+    fn make_instance_buffer(instances: &Vec<Instance>, device: &Device, _queue: &Queue) -> Buffer {
+        let instance_data: Vec<u8> = instances
+            .iter()
+            .map(|x| x.make_model_space_matrix())
+            .map(|x| {
+                vec![
+                    x.x.x.to_le_bytes(),
+                    x.x.y.to_le_bytes(),
+                    x.x.z.to_le_bytes(),
+                    x.x.w.to_le_bytes(),
+                    x.y.x.to_le_bytes(),
+                    x.y.y.to_le_bytes(),
+                    x.y.z.to_le_bytes(),
+                    x.y.w.to_le_bytes(),
+                    x.z.x.to_le_bytes(),
+                    x.z.y.to_le_bytes(),
+                    x.z.z.to_le_bytes(),
+                    x.z.w.to_le_bytes(),
+                    x.w.x.to_le_bytes(),
+                    x.w.y.to_le_bytes(),
+                    x.w.z.to_le_bytes(),
+                    x.w.w.to_le_bytes(),
+                ]
+            })
+            .flatten()
+            .flatten()
+            .collect();
+
+        device.create_buffer_init(&BufferInitDescriptor {
+            label: None,
+            contents: &instance_data,
+            usage: BufferUsages::VERTEX,
+        })
+    }
+
+    pub fn convert_instancing(instancing: &Instancing) -> Vec<Instance> {
+        match instancing {
+            Instancing::Single(i) => vec![Instance::from_descriptor(i)],
+            Instancing::Multiple(vi) => vi.iter().map(|x| Instance::from_descriptor(x)).collect(),
+        }
+    }
+
+    pub fn update_instance_buffer(&mut self, device: &Device, queue: &Queue) {
+        self.instance_buffer = Self::make_instance_buffer(&self.instances, device, queue);
     }
 
     pub fn mesh(&self) -> &Mesh {
@@ -116,5 +198,13 @@ impl Model {
 
     pub fn material(&self) -> &Material {
         &self.material
+    }
+
+    pub fn instances(&self) -> &Vec<Instance> {
+        &self.instances
+    }
+
+    pub fn instance_buffer(&self) -> &Buffer {
+        &self.instance_buffer
     }
 }
