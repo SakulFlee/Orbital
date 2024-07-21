eb2a732de5faf109d25a91faf0d03dc72c6f5b55
SakulFlee <dev@sakul-flee.de>
2024-07-21 21:11:35 +0200

    Added Camera related actions to WorldChange

--- Orbital/src/game/world/change.rs
+++ Orbital/src/game/world/change.rs
@@ -2,12 +2,16 @@ use std::{any::Any, fmt};
 
 use hashbrown::HashMap;
 
-use crate::{game::Element, resources::descriptors::ModelDescriptor, variant::Variant};
+use crate::{
+    game::Element,
+    resources::descriptors::{CameraDescriptor, ModelDescriptor},
+    variant::Variant,
+};
 
 use super::{ElementUlid, Identifier, ModelUlid};
 
 /// A [WorldChange] is a _proposed change to the [World]_.  
-/// 
+///
 /// [World]: super::World
 pub enum WorldChange {
     /// Queues an [Element] to be spawned.
@@ -21,24 +25,64 @@ pub enum WorldChange {
     /// Same as [WorldChange::SpawnModel], but without needing to supply
     /// an [ElementUlid].
     /// The [ElementUlid] of the current [Element] will be used.
-    /// 
+    ///
     /// [Model]: crate::resources::realizations::Model
     SpawnModelOwned(ModelDescriptor),
     /// Queues a [Model] to be spawned.
     ///
     /// Same as [WorldChange::SpawnModelOwned], but with needing to supply
     /// an [ElementUlid].
-    /// 
+    ///
     /// [Model]: crate::resources::realizations::Model
     SpawnModel(ModelDescriptor, ElementUlid),
     /// Queues a [Model] to be despawned.  
     /// Use a [ModelUlid] to specify which is being despawned.
-    /// 
+    ///
     /// [Model]: crate::resources::realizations::Model
     DespawnModel(ModelUlid),
     /// Sends a message to one or many [Elements](Element).  
     /// The message must be a [HashMap<String, Variant>].
     SendMessage(Identifier, HashMap<String, Variant>),
+    /// Spawns a [Camera] with a given [CameraDescriptor].  
+    /// If the chosen `identifier` of the [Camera] is already taken, this change
+    /// will be rejected.
+    ///
+    /// [Camera]: crate::resources::realizations::Camera
+    SpawnCamera(CameraDescriptor),
+    /// Similar to [Self::SpawnCamera], but also makes the new [Camera] active.
+    ///
+    /// [Camera]: crate::resources::realizations::Camera
+    SpawnCameraAndMakeActive(CameraDescriptor),
+    /// Despawns a [Camera] given a `identifier` ([String]).
+    ///
+    /// If a [Camera] with the given `identifier` exists, it will be removed.  
+    /// If a [Camera] with the given `identifier` does not exist, nothing will happen.
+    ///
+    /// If the active [Camera] is removed, a [Default] [Camera] will be spawned
+    /// automatically on the next cycle.
+    ///
+    /// [Camera]: crate::resources::realizations::Camera
+    DespawnCamera(String),
+    /// Changes the active [Camera] to a [Camera] with the
+    /// given `identifier` ([String]).
+    ///
+    /// If a [Camera] with the given `identifier` does **not** exist,
+    /// the active [Camera] **will not be changed**.
+    ///
+    /// [Camera]: crate::resources::realizations::Camera
+    ChangeActiveCamera(String),
+    /// Applies a given [CameraDescriptor] properties to a [Camera].
+    ///
+    /// If a [Camera] exists with the specified [CameraDescriptor::identifier],
+    /// any property of the [CameraDescriptor] will be applied to the [Camera]
+    /// and a [Buffer] update is triggered.  
+    /// If a [Camera] does not exists with the [CameraDescriptor::identifier],
+    /// this world change will be rejected and a warning will be printed to
+    /// console.
+    ///
+    /// [Camera]: crate::resources::realizations::Camera
+    /// [Buffer]: wgpu::Buffer
+    UpdateCamera(CameraDescriptor),
 }
 
 impl fmt::Debug for WorldChange {
@@ -56,6 +100,16 @@ impl fmt::Debug for WorldChange {
                 .field(arg0)
                 .field(arg1)
                 .finish(),
+            Self::SpawnCamera(arg0) => f.debug_tuple("SpawnCamera").field(arg0).finish(),
+            Self::SpawnCameraAndMakeActive(arg0) => f
+                .debug_tuple("SpawnCameraAndMakeActive")
+                .field(arg0)
+                .finish(),
+            Self::DespawnCamera(arg0) => f.debug_tuple("DespawnCamera").field(arg0).finish(),
+            Self::ChangeActiveCamera(arg0) => {
+                f.debug_tuple("ChangeActiveCamera").field(arg0).finish()
+            }
+            Self::UpdateCamera(arg0) => f.debug_tuple("UpdateCamera").field(arg0).finish(),
         }
     }
 }
