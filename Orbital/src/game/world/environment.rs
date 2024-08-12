b21978a226ff5c6e9814954c3acd0246db7cd18e
SakulFlee <dev@sakul-flee.de>
2024-08-13 00:22:39 +0200

    Added radiance and irradiance to world env

--- Orbital/src/game/world/environment.rs
+++ Orbital/src/game/world/environment.rs
@@ -3,13 +3,21 @@ use crate::resources::descriptors::{CubeTextureDescriptor, MaterialDescriptor};
 #[derive(Debug)]
 pub struct WorldEnvironment {
     pub sky_box_material_descriptor: MaterialDescriptor,
+    pub radiance_material_descriptor: MaterialDescriptor,
+    pub irradiance_material_descriptor: MaterialDescriptor,
 }
 
 impl WorldEnvironment {
-    pub fn new(sky_box_material_descriptor: MaterialDescriptor) -> Self {
+    pub fn new(
+        sky_box_material_descriptor: MaterialDescriptor,
+        radiance_material_descriptor: MaterialDescriptor,
+        irradiance_material_descriptor: MaterialDescriptor,
+    ) -> Self {
         if let MaterialDescriptor::SkyBox { sky_texture: _ } = &sky_box_material_descriptor {
             Self {
                 sky_box_material_descriptor,
+                radiance_material_descriptor,
+                irradiance_material_descriptor,
             }
         } else {
             panic!("WorldEnvironment requires the SkyBox Material to be of type MaterialDescriptor::SkyBox!");
@@ -28,6 +36,18 @@ impl Default for WorldEnvironment {
                 sky_texture: CubeTextureDescriptor::RadianceHDRFile {
                     cube_face_size: 1024,
                     path: "Assets/HDRs/lonely_road_afternoon_puresky_4k.hdr",
+                },
+            },
+            radiance_material_descriptor: MaterialDescriptor::SkyBox {
+                sky_texture: CubeTextureDescriptor::RadianceHDRFile {
+                    cube_face_size: 32,
+                    path: "Assets/HDRs/lonely_road_afternoon_puresky_radiance.hdr",
+                },
+            },
+            irradiance_material_descriptor: MaterialDescriptor::SkyBox {
+                sky_texture: CubeTextureDescriptor::RadianceHDRFile {
+                    cube_face_size: 32,
+                    path: "Assets/HDRs/lonely_road_afternoon_puresky_irradiance.hdr",
                 },
             },
         }
