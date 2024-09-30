e0e982547bde4806365e383f42cb6897feaadac4
SakulFlee <dev@sakul-flee.de>
2024-09-30 22:30:28 +0200

    Space out lines in a line for helmet to "hit"

--- Example/src/game/elements/lights.rs
+++ Example/src/game/elements/lights.rs
@@ -12,23 +12,28 @@ impl Element for Lights {
         ElementRegistration::new("Lights").with_initial_world_changes(vec![
             WorldChange::SpawnLight(LightDescriptor::PointLight(PointLight {
                 label: "Scene Lights".into(),
-                position: Vector3::new(5.0, 0.0, 0.0),
+                position: Vector3::new(0.0, 0.0, 5.0),
                 color: Vector3::new(1.0, 1.0, 1.0),
             })),
             WorldChange::SpawnLight(LightDescriptor::PointLight(PointLight {
                 label: "Scene Lights".into(),
-                position: Vector3::new(5.0, 5.0, 5.0),
+                position: Vector3::new(0.0, 0.0, 10.0),
                 color: Vector3::new(1.0, 0.0, 0.0),
             })),
             WorldChange::SpawnLight(LightDescriptor::PointLight(PointLight {
-                position: Vector3::new(5.0, 5.0, -5.0),
+                position: Vector3::new(0.0, 0.0, 15.0),
                 label: "Scene Lights".into(),
                 color: Vector3::new(0.0, 1.0, 0.0),
             })),
             WorldChange::SpawnLight(LightDescriptor::PointLight(PointLight {
                 label: "Scene Lights".into(),
-                position: Vector3::new(5.0, -5.0, -5.0),
+                position: Vector3::new(0.0, 0.0, 20.0),
                 color: Vector3::new(0.0, 0.0, 1.0),
+            })),
+            WorldChange::SpawnLight(LightDescriptor::PointLight(PointLight {
+                label: "Scene Lights".into(),
+                position: Vector3::new(0.0, 0.0, 25.0),
+                color: Vector3::new(1.0, 1.0, 1.0),
             })),
         ])
     }
