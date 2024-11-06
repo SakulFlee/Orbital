1b86b65b3e6184d3f5db84a0c4a5aa5a7386fcbf
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2024-11-06 17:30:50 +0100

    Cache optimization: TextureDescriptor Fixes #365

--- Orbital/src/resources/descriptors/texture.rs
+++ Orbital/src/resources/descriptors/texture.rs
@@ -1,6 +1,6 @@
 use cgmath::{Vector2, Vector4};
 
-#[derive(Debug, Clone, Hash, Eq, PartialEq)]
+#[derive(Debug, Clone, Hash)]
 pub enum TextureDescriptor {
     /// Creates a texture by loading it from a file.
     /// ⚠️ This file must be accessible during runtime!
@@ -28,7 +28,7 @@ pub enum TextureDescriptor {
     ///
     /// ```ignore
     /// # use orbital::cgmath::Vector4;
-    /// 
+    ///
     /// let channel_{x, y, z, w}: f64 = Vector4::{x, y, z, w} / 255.0f64;
     /// ```
     ///
@@ -37,7 +37,7 @@ pub enum TextureDescriptor {
     /// 100% Red:
     /// ```ignore
     /// # use orbital::cgmath::Vector4;
-    /// 
+    ///
     /// Vector4 {
     ///     x: 255,
     ///     y: 0,
@@ -49,7 +49,7 @@ pub enum TextureDescriptor {
     /// 33.3% Red, 33.3% Blue, 33.3% Blue:
     /// ```ignore
     /// # use orbital::cgmath::Vector4;
-    /// 
+    ///
     /// Vector4 {
     ///     x: 85,
     ///     y: 85,
@@ -90,3 +90,40 @@ impl TextureDescriptor {
     pub const UNIFORM_LUMA_WHITE: Self = Self::UniformLuma { data: 255 };
     pub const UNIFORM_LUMA_GRAY: Self = Self::UniformLuma { data: 128 };
 }
+
+impl PartialEq for TextureDescriptor {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Self::FilePath(l0), Self::FilePath(r0)) => l0 == r0,
+            (Self::StandardSRGBAu8Data(l0, l1), Self::StandardSRGBAu8Data(r0, r1)) => {
+                // Note: This should be much faster than just doing `l0 == r0 && l1 == r1` as this **fails fast**!
+                // We first compare the obvious facts, like the size of the data as well as the resolution of the texture.
+                // If those actually are equal, which already might be rare, we compare byte-by-byte but in an iterator and with fail fast enabled (any).
+                // Thus, as soon as a single byte is not equal, we fail the equality check and saving a lot of time this way!
+                if l1 != r1 || l0.len() != r0.len() {
+                    return false;
+                }
+
+                l0.iter().zip(r0.iter()).any(|(l, r)| l.eq(r))
+            }
+            (Self::UniformColor(l0), Self::UniformColor(r0)) => l0 == r0,
+            (
+                Self::Luma {
+                    data: l_data,
+                    size: l_size,
+                },
+                Self::Luma {
+                    data: r_data,
+                    size: r_size,
+                },
+            ) => l_data == r_data && l_size == r_size,
+            (Self::UniformLuma { data: l_data }, Self::UniformLuma { data: r_data }) => {
+                l_data == r_data
+            }
+            (Self::Depth(l0), Self::Depth(r0)) => l0 == r0,
+            _ => false,
+        }
+    }
+}
+
+impl Eq for TextureDescriptor {}
