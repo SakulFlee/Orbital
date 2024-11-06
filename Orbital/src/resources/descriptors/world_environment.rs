36ce0e8113f2284e0d1b00db29df5b12084ad202
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2024-11-06 17:31:07 +0100

    Cache optimization: WorldEnvironmentDescriptor Fixes #365

--- Orbital/src/resources/descriptors/world_environment.rs
+++ Orbital/src/resources/descriptors/world_environment.rs
@@ -2,7 +2,7 @@ use cgmath::Vector2;
 
 use super::{SamplingType, SkyboxType};
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Hash, Eq)]
 pub enum WorldEnvironmentDescriptor {
     /// Loading an HDRI from file.  
     /// First of all, will convert the HDRI _equirectangular_ image
@@ -52,6 +52,61 @@ impl WorldEnvironmentDescriptor {
             } => {
                 *skybox_type = new_skybox_type;
             }
+        }
+    }
+}
+
+impl PartialEq for WorldEnvironmentDescriptor {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (
+                Self::FromFile {
+                    skybox_type: l_skybox_type,
+                    cube_face_size: l_cube_face_size,
+                    path: l_path,
+                    sampling_type: l_sampling_type,
+                },
+                Self::FromFile {
+                    skybox_type: r_skybox_type,
+                    cube_face_size: r_cube_face_size,
+                    path: r_path,
+                    sampling_type: r_sampling_type,
+                },
+            ) => {
+                l_skybox_type == r_skybox_type
+                    && l_cube_face_size == r_cube_face_size
+                    && l_path == r_path
+                    && l_sampling_type == r_sampling_type
+            }
+            (
+                Self::FromData {
+                    skybox_type: l_skybox_type,
+                    cube_face_size: l_cube_face_size,
+                    data: l_data,
+                    size: l_size,
+                    sampling_type: l_sampling_type,
+                },
+                Self::FromData {
+                    skybox_type: r_skybox_type,
+                    cube_face_size: r_cube_face_size,
+                    data: r_data,
+                    size: r_size,
+                    sampling_type: r_sampling_type,
+                },
+            ) => {
+                // Check for obvious facts first.
+                if !(l_skybox_type == r_skybox_type
+                    && l_cube_face_size == r_cube_face_size
+                    && l_size == r_size
+                    && l_sampling_type == r_sampling_type)
+                {
+                    return false;
+                }
+
+                // Then, compare byte-by-byte with fail-fast.
+                return l_data.iter().zip(r_data.iter()).any(|(l, r)| l.eq(r));
+            }
+            _ => false,
         }
     }
 }
