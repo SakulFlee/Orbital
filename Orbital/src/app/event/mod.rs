d07ee3e748c909c028712f830cf7c314890e9213
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2024-11-20 19:54:01 +0100

    Removed unused entries from AppEvent and added FocusChanged

--- Orbital/src/app/event/mod.rs
+++ Orbital/src/app/event/mod.rs
@@ -2,7 +2,6 @@ use std::sync::Arc;
 
 use cgmath::Vector2;
 use wgpu::{Device, Queue, SurfaceConfiguration, TextureView};
-use winit::event::{DeviceEvent, DeviceId, StartCause, WindowEvent};
 
 mod input;
 pub use input::*;
@@ -12,10 +11,7 @@ pub enum AppEvent {
     Resumed(SurfaceConfiguration, Arc<Device>, Arc<Queue>),
     Suspended,
     InputEvent(InputEvent),
-    WindowEvent(WindowEvent),
-    NewEventCycle(StartCause),
-    DeviceEvent(DeviceId, DeviceEvent),
-    MemoryWarning,
+    FocusChange { focused: bool },
     Resize(Vector2<u32>, Arc<Device>, Arc<Queue>),
     Render(TextureView, Arc<Device>, Arc<Queue>),
     Update,
