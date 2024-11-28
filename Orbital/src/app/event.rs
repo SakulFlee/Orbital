6ac1bf270310b639ef6ef80b916f60b3d3401666
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2024-11-28 21:23:08 +0100

    Moved AppEvent

--- Orbital/src/app/event.rs
+++ Orbital/src/app/event.rs
@@ -3,16 +3,14 @@ use std::sync::Arc;
 use cgmath::Vector2;
 use wgpu::{Device, Queue, SurfaceConfiguration, SurfaceTexture, TextureView};
 
-mod input;
-pub use input::*;
+use crate::input::InputState;
 
 #[derive(Debug)]
 pub enum AppEvent {
     Resumed(SurfaceConfiguration, Arc<Device>, Arc<Queue>),
     Suspended,
-    InputEvent(InputEvent),
     FocusChange { focused: bool },
     Resize(Vector2<u32>, Arc<Device>, Arc<Queue>),
     Render(SurfaceTexture, TextureView, Arc<Device>, Arc<Queue>),
-    Update,
+    Update(InputState),
 }
