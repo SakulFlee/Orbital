438f3618c9351cdd098d77e635315d9e994d6399
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2024-11-28 21:22:14 +0100

    Added InputState

---
+++ Orbital/src/input/state.rs
@@ -0,0 +1,237 @@
+use cgmath::{Vector2, Zero};
+use gilrs::Axis;
+use hashbrown::HashMap;
+use log::debug;
+use winit::event::{ElementState, MouseScrollDelta};
+
+use super::{InputAxis, InputButton, InputEvent, InputId};
+
+#[derive(Debug, Clone)]
+pub struct InputState {
+    button_states: HashMap<InputId, HashMap<InputButton, bool>>,
+    delta_states: HashMap<InputId, HashMap<InputAxis, Vector2<f64>>>,
+    mouse_cursor_position_state: Vector2<f64>,
+}
+
+impl InputState {
+    pub fn new() -> Self {
+        Self {
+            button_states: HashMap::new(),
+            delta_states: HashMap::new(),
+            mouse_cursor_position_state: Vector2::zero(),
+        }
+    }
+
+    pub fn reset_delta_states(&mut self) {
+        self.delta_states.iter_mut().for_each(|(_, state)| {
+            state
+                .iter_mut()
+                .for_each(|(_, delta)| *delta = Vector2::zero())
+        });
+    }
+
+    /// Handles input events to populate the input state.
+    pub fn handle_event(&mut self, input_event: InputEvent) {
+        let (input_id, input_button_state, input_axis_state): (
+            InputId,
+            Option<(InputButton, bool)>,
+            Option<(InputAxis, Vector2<f64>)>,
+        ) = match input_event {
+            InputEvent::KeyboardButton {
+                device_id,
+                event,
+                is_synthetic: _,
+            } => (
+                InputId::KeyboardOrMouse(device_id),
+                Some((
+                    InputButton::Keyboard(event.physical_key),
+                    event.state == ElementState::Pressed,
+                )),
+                None,
+            ),
+            InputEvent::MouseButton {
+                device_id,
+                state,
+                button,
+            } => (
+                InputId::KeyboardOrMouse(device_id),
+                Some((InputButton::Mouse(button), state == ElementState::Pressed)),
+                None,
+            ),
+            InputEvent::MouseWheel {
+                device_id,
+                delta,
+                phase: _,
+            } => {
+                let vector_delta = match delta {
+                    MouseScrollDelta::LineDelta(x, y) => Vector2::new(x as f64, y as f64),
+                    MouseScrollDelta::PixelDelta(physical_position) => {
+                        Vector2::new(physical_position.x, physical_position.y)
+                    }
+                };
+
+                (
+                    InputId::KeyboardOrMouse(device_id),
+                    None,
+                    Some((InputAxis::MouseScrollWheel, vector_delta)),
+                )
+            }
+            InputEvent::MouseMovedPosition {
+                device_id: _,
+                position,
+            } => {
+                let vector_delta = Vector2::new(position.x, position.y);
+
+                self.mouse_cursor_position_state = vector_delta;
+                return; // No further processing required!
+            }
+            InputEvent::MouseMovedDelta { device_id, delta } => {
+                let vector_delta = Vector2::new(delta.0, delta.1);
+
+                (
+                    InputId::KeyboardOrMouse(device_id),
+                    None,
+                    Some((InputAxis::MouseMovement, vector_delta)),
+                )
+            }
+            #[cfg(feature = "gamepad_input")]
+            InputEvent::GamepadButton {
+                gamepad_id,
+                button,
+                pressed,
+            } => (
+                InputId::Gamepad(gamepad_id),
+                Some((InputButton::Gamepad(button), pressed)),
+                None,
+            ),
+            #[cfg(feature = "gamepad_input")]
+            InputEvent::GamepadAxis {
+                gamepad_id,
+                axis,
+                value,
+            } => {
+                let (axis, vector) = match axis {
+                    Axis::LeftStickX => {
+                        (InputAxis::GamepadLeftStick, Vector2::new(value as f64, 0.0))
+                    }
+                    Axis::LeftStickY => {
+                        (InputAxis::GamepadLeftStick, Vector2::new(0.0, value as f64))
+                    }
+                    Axis::RightStickX => (
+                        InputAxis::GamepadRightStick,
+                        Vector2::new(value as f64, 0.0),
+                    ),
+                    Axis::RightStickY => (
+                        InputAxis::GamepadRightStick,
+                        Vector2::new(value as f64, 0.0),
+                    ),
+                    Axis::LeftZ => (InputAxis::GamepadTrigger, Vector2::new(value as f64, 0.0)),
+                    Axis::RightZ => (InputAxis::GamepadTrigger, Vector2::new(0.0, value as f64)),
+                    _ => return,
+                };
+
+                (InputId::Gamepad(gamepad_id), None, Some((axis, vector)))
+            }
+            // Nothing to do, so just return out of here :)
+            _ => return,
+        };
+
+        if let Some((button, pressed)) = input_button_state {
+            self.button_states
+                .entry(input_id)
+                .or_insert(HashMap::new())
+                .entry(button)
+                .and_modify(|x| *x = pressed)
+                .or_insert(pressed);
+        } else if let Some((axis, delta)) = input_axis_state {
+            self.delta_states
+                .entry(input_id)
+                .or_insert(HashMap::new())
+                .entry(axis)
+                .and_modify(|x| *x = delta)
+                .or_insert(delta);
+        }
+    }
+
+    pub fn button_state_specific(
+        &self,
+        input_button: &InputButton,
+        input_id: InputId,
+    ) -> Option<bool> {
+        self.button_states
+            .get(&input_id)
+            .and_then(|x| x.get(input_button))
+            .cloned()
+    }
+
+    pub fn button_state_any(&self, input_button: &InputButton) -> Option<(InputId, bool)> {
+        self.button_states
+            .iter()
+            .find(|(_, state)| state.contains_key(input_button))
+            .map(|(input_id, state)| {
+                if let Some(pressed) = state.get(input_button) {
+                    Some((input_id.clone(), pressed.clone()))
+                } else {
+                    None
+                }
+            })
+            .flatten()
+    }
+
+    pub fn button_state_all(&self, input_button: &InputButton) -> Vec<(InputId, bool)> {
+        self.button_states
+            .iter()
+            .filter(|(_, state)| state.contains_key(input_button))
+            .filter_map(|(input_id, state)| {
+                if let Some(pressed) = state.get(input_button) {
+                    Some((input_id.clone(), pressed.clone()))
+                } else {
+                    None
+                }
+            })
+            .collect()
+    }
+
+    pub fn delta_state_specific(
+        &self,
+        input_axis: &InputAxis,
+        input_id: InputId,
+    ) -> Option<Vector2<f64>> {
+        self.delta_states
+            .get(&input_id)
+            .and_then(|x| x.get(input_axis))
+            .cloned()
+    }
+
+    pub fn delta_state_any(&self, input_axis: &InputAxis) -> Option<(InputId, Vector2<f64>)> {
+        self.delta_states
+            .iter()
+            .find(|(_, state)| state.contains_key(input_axis))
+            .map(|(input_id, state)| {
+                if let Some(delta) = state.get(input_axis) {
+                    Some((input_id.clone(), delta.clone()))
+                } else {
+                    None
+                }
+            })
+            .flatten()
+    }
+
+    pub fn delta_state_all(&self, input_axis: &InputAxis) -> Vec<(InputId, Vector2<f64>)> {
+        self.delta_states
+            .iter()
+            .filter(|(_, state)| state.contains_key(input_axis))
+            .filter_map(|(input_id, state)| {
+                if let Some(delta) = state.get(input_axis) {
+                    Some((input_id.clone(), delta.clone()))
+                } else {
+                    None
+                }
+            })
+            .collect()
+    }
+
+    pub fn mouse_cursor_position_state(&self) -> Vector2<f64> {
+        self.mouse_cursor_position_state
+    }
+}
