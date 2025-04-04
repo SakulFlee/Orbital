39eeecea472c3d35ac4a5f901808f6cc087a23da
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2025-04-04 19:14:41 +0200

    Updated CacheState to use new MaterialShader system

--- Orbital/src/cache_state.rs
+++ Orbital/src/cache_state.rs
@@ -1,22 +1,18 @@
 use std::{cell::RefCell, sync::Arc};
 
+use material_shader::MaterialShaderDescriptor;
+
 use crate::{
     cache::Cache,
     resources::{
-        descriptors::{
-            MaterialDescriptor, MeshDescriptor, PipelineDescriptor, ShaderDescriptor,
-            TextureDescriptor,
-        },
+        descriptors::MeshDescriptor,
         realizations::{Material, Mesh, Pipeline, Shader, Texture},
     },
 };
 
 pub struct CacheState {
     pub mesh_cache: RefCell<Cache<Arc<MeshDescriptor>, Mesh>>,
-    pub material_cache: RefCell<Cache<Arc<MaterialDescriptor>, Material>>,
-    pub texture_cache: RefCell<Cache<Arc<TextureDescriptor>, Texture>>,
-    pub pipeline_cache: RefCell<Cache<Arc<PipelineDescriptor>, Pipeline>>,
-    pub shader_cache: RefCell<Cache<Arc<ShaderDescriptor>, Shader>>,
+    pub material_cache: RefCell<Cache<Arc<MaterialShaderDescriptor>, Material>>,
 }
 
 impl CacheState {
@@ -24,9 +20,6 @@ impl CacheState {
         Self {
             mesh_cache: RefCell::new(Cache::new()),
             material_cache: RefCell::new(Cache::new()),
-            texture_cache: RefCell::new(Cache::new()),
-            pipeline_cache: RefCell::new(Cache::new()),
-            shader_cache: RefCell::new(Cache::new()),
         }
     }
 }
