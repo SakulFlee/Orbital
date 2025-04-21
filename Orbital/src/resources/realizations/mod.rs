0ea91ba60d96508f5ff13e15616c40d6eb1cb31c
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2025-04-21 16:47:44 +0200

    Cleanup

--- Orbital/src/resources/realizations/mod.rs
+++ Orbital/src/resources/realizations/mod.rs
@@ -1,16 +1,12 @@
 pub mod bounding_box;
 pub mod camera;
-pub mod ibl_brdf;
 pub mod instance;
 pub mod light;
 pub mod mesh;
 pub mod model;
-pub mod world_environment;
 
 pub use bounding_box::*;
 pub use camera::*;
-pub use ibl_brdf::*;
 pub use light::*;
 pub use mesh::*;
 pub use model::*;
-pub use world_environment::*;
