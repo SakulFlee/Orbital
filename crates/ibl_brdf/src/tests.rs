ed242f889a7478ef266021cf449364b101ff2b69
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2025-04-21 16:46:35 +0200

    IBL BRDF: Sharding

---
+++ crates/ibl_brdf/src/tests.rs
@@ -0,0 +1,32 @@
+use cgmath::Vector2;
+use wgpu::{TextureFormat, TextureUsages};
+
+use crate::IblBrdf;
+
+#[test]
+fn test_empty() {
+    let (_, device, _) = wgpu_test_adapter::make_wgpu_connection();
+
+    const SIZE: u32 = 512;
+
+    let result = IblBrdf::create_empty(
+        Some("Test"),
+        Vector2::new(SIZE, SIZE),
+        TextureFormat::Rgba8UnormSrgb,
+        TextureUsages::TEXTURE_BINDING,
+        &device,
+    );
+    let _ = result
+        .texture
+        .expect("Texture must exist after generation!");
+}
+
+#[test]
+fn test_generate() {
+    let (_, device, queue) = wgpu_test_adapter::make_wgpu_connection();
+
+    let result = IblBrdf::generate(&device, &queue);
+    let _ = result
+        .texture
+        .expect("Texture must exist after generation!");
+}
