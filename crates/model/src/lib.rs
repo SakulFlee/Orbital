61ed6cf9388e5fab67d5b694e825063d8a7b1f22
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2025-04-24 19:06:52 +0200

    Model: Sharding

---
+++ crates/model/src/lib.rs
@@ -0,0 +1,124 @@
+use std::{cell::RefCell, sync::Arc};
+
+use wgpu::{
+    Buffer, BufferUsages, Device, Queue, TextureFormat,
+    util::{BufferInitDescriptor, DeviceExt},
+};
+
+pub use cache::{Cache, CacheEntry};
+pub use instance::Instance;
+pub use material_shader::{Error, MaterialShader, MaterialShaderDescriptor};
+pub use mesh::{Mesh, MeshDescriptor};
+
+mod descriptor;
+pub use descriptor::*;
+
+#[cfg(test)]
+mod tests;
+
+#[derive(Debug)]
+pub struct Model {
+    mesh: Arc<Mesh>,
+    materials: Vec<Arc<MaterialShader>>,
+    instance_count: u32,
+    instance_buffer: Buffer,
+}
+
+impl Model {
+    pub fn from_descriptor(
+        descriptor: &ModelDescriptor,
+        surface_format: &TextureFormat,
+        device: &Device,
+        queue: &Queue,
+        with_mesh_cache: Option<&RefCell<Cache<Arc<MeshDescriptor>, Mesh>>>,
+        with_material_cache: Option<&RefCell<Cache<Arc<MaterialShaderDescriptor>, MaterialShader>>>,
+    ) -> Result<Self, Error> {
+        // --- Mesh ---
+        let mesh = if let Some(cache) = with_mesh_cache {
+            cache
+                .borrow_mut()
+                .entry(descriptor.mesh.clone())
+                .or_insert(CacheEntry::new(Mesh::from_descriptor(
+                    &descriptor.mesh,
+                    device,
+                    queue,
+                )))
+                .clone_inner()
+        } else {
+            Arc::new(Mesh::from_descriptor(&descriptor.mesh, device, queue))
+        };
+
+        // --- Material ---
+        let mut materials = Vec::new();
+        if let Some(cache) = with_material_cache {
+            for material_descriptor in &descriptor.materials {
+                materials.push(
+                    cache
+                        .borrow_mut()
+                        .entry(material_descriptor.clone())
+                        .or_insert(CacheEntry::new(MaterialShader::from_descriptor(
+                            &material_descriptor,
+                            Some(*surface_format),
+                            device,
+                            queue,
+                        )?))
+                        .clone_inner(),
+                );
+            }
+        } else {
+            for material_descriptor in &descriptor.materials {
+                materials.push(Arc::new(MaterialShader::from_descriptor(
+                    &material_descriptor,
+                    Some(*surface_format),
+                    device,
+                    queue,
+                )?));
+            }
+        }
+
+        // --- Instances ---
+        // Take Transform count == Instance count
+        let instance_count = descriptor.transforms.len() as u32;
+
+        // Turn Transforms into Instances
+        let instances = descriptor
+            .transforms
+            .iter()
+            .map(Instance::from)
+            .collect::<Vec<_>>();
+        // Turn Instances into buffer data (bytes)
+        let instance_buffer_data: Vec<u8> = instances
+            .iter()
+            .flat_map(|x| x.to_buffer_data())
+            .flatten()
+            .collect();
+        let instance_buffer = device.create_buffer_init(&BufferInitDescriptor {
+            label: Some("Model Instance Buffer"),
+            contents: &instance_buffer_data,
+            usage: BufferUsages::VERTEX,
+        });
+
+        Ok(Self {
+            mesh,
+            materials,
+            instance_count,
+            instance_buffer,
+        })
+    }
+
+    pub fn mesh(&self) -> &Mesh {
+        &self.mesh
+    }
+
+    pub fn materials(&self) -> &Vec<Arc<MaterialShader>> {
+        &self.materials
+    }
+
+    pub fn instance_count(&self) -> u32 {
+        self.instance_count
+    }
+
+    pub fn instance_buffer(&self) -> &Buffer {
+        &self.instance_buffer
+    }
+}
