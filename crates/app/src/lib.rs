40ea5f84c0df0d149089229269c110f77aa8ba5c
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2025-04-27 00:23:47 +0200

    App: Removed broken examples in doctest

--- crates/app/src/lib.rs
+++ crates/app/src/lib.rs
@@ -42,27 +42,7 @@ use input::*;
 /// To use an [App], all you need to do is call the
 /// [AppRuntime] with your implementation, like so:
 ///
-/// ```rust
-/// # use orbital::{app::{AppSettings, AppRuntime, App}, wgpu::{Device, Queue, SurfaceConfiguration}, winit::event_loop::EventLoop};
-///
-/// # struct MyApp;
-/// # impl App for MyApp {
-/// #   fn init(config: &SurfaceConfiguration, device: &Device, queue: &Queue) -> Self
-/// #   where Self: Sized,
-/// #   {
-/// #       std::process::exit(0);
-/// #       Self {}
-/// #   }
-/// # }
-///
-/// # fn main() {
-/// let event_loop = EventLoop::new().unwrap(); // Acquire event loop, might be different based on platform!
-/// let settings = AppSettings::default();
-///
-/// AppRuntime::<MyApp>::liftoff(event_loop, settings)
-///     .expect("Runtime failure");
-/// # }
-/// ```
+/// TODO: New example needed
 ///
 /// You will need three things:
 ///
@@ -75,25 +55,7 @@ use input::*;
 /// Getting an implementation of [App] should be straight forward.
 /// Make a structure and implement the trait like so:
 ///
-/// ```rust
-/// # use orbital::{
-/// #     app::{App, AppChange},
-/// #     wgpu::{Device, Queue, SurfaceConfiguration}
-/// # };
-///
-/// pub struct MyApp;
-///
-/// impl App for MyApp {
-///     // ...
-/// #    fn init(config: &SurfaceConfiguration, device: &Device, _queue: &Queue) -> Self
-/// #    where
-/// #        Self: Sized,
-/// #    {
-/// #       std::process::exit(0);
-/// #       Self {}
-/// #    }
-/// }
-/// ```
+/// TODO: New example needed
 ///
 /// Each function should be straight forward and easy to understand.
 /// Not every function needs to be implemented, many have default
@@ -117,154 +79,7 @@ use input::*;
 /// Things like window name or initial size can be configured.
 /// The default settings are enough to get started.
 ///
-/// # Examples
-///
-/// ## [Game] & [GameRuntime] as an example
-/// For a fully integrated example take a look at [Game]
-/// and [GameRuntime].
-/// Both build on top of an [App].
-///
-/// ## RGB Triangle with Shader
-///
-/// ![Example image](https://raw.githubusercontent.com/SakulFlee/Orbital/main/.github/images/app_example_triangle.png)
-///
-/// ```rust
-/// // app.rs
-/// use orbital::{
-///     app::{App, AppChange},
-///     wgpu::{
-///         Color, CommandEncoderDescriptor, Device, FragmentState, LoadOp, MultisampleState,
-///         Operations, PipelineLayoutDescriptor, PrimitiveState, Queue, RenderPassColorAttachment,
-///         RenderPassDescriptor, RenderPipeline, RenderPipelineDescriptor, ShaderModuleDescriptor,
-///         ShaderSource, StoreOp, SurfaceConfiguration, TextureView, VertexState,
-///     },
-/// };
-///
-/// pub struct TriangleApp {
-///     // Note: Curiously, the following two variables don't have to be stored if
-///     // we are just referencing them. Uncomment the below and the end of
-///     // Self::init if you need access to them :)
-///     //
-///     // shader: ShaderModule,
-///     // pipeline_layout: PipelineLayout,
-///     pipeline: RenderPipeline,
-/// }
-///
-/// impl App for TriangleApp {
-///     fn init(config: &SurfaceConfiguration, device: &Device, _queue: &Queue) -> Self
-///     where
-///         Self: Sized,
-///     {
-///         # std::process::exit(0);
-///         let shader = device.create_shader_module(ShaderModuleDescriptor {
-///             label: None,
-///             source: ShaderSource::Wgsl(include_str!("your_shader_here.wgsl").into()),
-///         });
-///
-///         let pipeline_layout = device.create_pipeline_layout(&PipelineLayoutDescriptor {
-///             label: None,
-///             bind_group_layouts: &[],
-///             push_constant_ranges: &[],
-///         });
-///
-///         let pipeline = device.create_render_pipeline(&RenderPipelineDescriptor {
-///             label: None,
-///             layout: Some(&pipeline_layout),
-///             vertex: VertexState {
-///                 module: &shader,
-///                 entry_point: "vs_main",
-///                 buffers: &[],
-///                 compilation_options: Default::default(),
-///             },
-///             fragment: Some(FragmentState {
-///                 module: &shader,
-///                 entry_point: "fs_main",
-///                 targets: &[Some(config.format.into())],
-///                 compilation_options: Default::default(),
-///             }),
-///             primitive: PrimitiveState::default(),
-///             depth_stencil: None,
-///             multisample: MultisampleState::default(),
-///             multiview: None,
-///         });
-///
-///         Self {
-///             // Note: Check variable description in struct declaration!
-///             //
-///             // shader,
-///             // pipeline_layout,
-///             pipeline,
-///         }
-///     }
-///
-///     fn on_update(&mut self) -> Option<Vec<AppChange>> {
-///         // Nothing needed for this example!
-///         // All events that we care about are already taken care of.
-///
-///         None
-///     }
-///
-///     fn on_render(&mut self, view: &TextureView, device: &Device, queue: &Queue) {
-///         let mut encoder = device.create_command_encoder(&CommandEncoderDescriptor { label: None });
-///
-///         {
-///             let mut render_pass = encoder.begin_render_pass(&RenderPassDescriptor {
-///                 label: None,
-///                 color_attachments: &[Some(RenderPassColorAttachment {
-///                     view,
-///                     resolve_target: None,
-///                     ops: Operations {
-///                         load: LoadOp::Clear(Color::BLACK),
-///                         store: StoreOp::Store,
-///                     },
-///                 })],
-///                 depth_stencil_attachment: None,
-///                 timestamp_writes: None,
-///                 occlusion_query_set: None,
-///             });
-///             render_pass.set_pipeline(&self.pipeline);
-///             render_pass.draw(0..3, 0..1);
-///         }
-///
-///         queue.submit(Some(encoder.finish()));
-///     }
-/// }
-/// ```
-///
-/// ```wgsl
-/// // shader.wgsl
-/// struct VertexOutput {
-///     @builtin(position) clip_position: vec4<f32>,
-///     @location(0) color: vec4<f32>,
-/// };
-///
-/// @vertex
-/// fn vs_main(
-///     @builtin(vertex_index) in_vertex_index: u32
-/// ) -> VertexOutput {
-///     var out: VertexOutput;
-///
-///     let x = f32(1 - i32(in_vertex_index)) * 0.5;
-///     let y = f32(i32(in_vertex_index & 1u) * 2 - 1) * 0.5;
-///
-///     out.clip_position = vec4<f32>(x, y, 0.0, 1.0);
-///
-///     if in_vertex_index == 0 {
-///         out.color = vec4<f32>(1.0, 0.0, 0.0, 1.0);
-///     } else if in_vertex_index == 1 {
-///         out.color = vec4<f32>(0.0, 1.0, 0.0, 1.0);
-///     } else if in_vertex_index == 2 {
-///         out.color = vec4<f32>(0.0, 0.0, 1.0, 1.0);
-///     }
-///
-///     return out;
-/// }
-///
-/// @fragment
-/// fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
-///     return in.color;
-/// }
-/// ```
+/// TODO : Needs new example or similar description
 ///
 /// [WASM]: https://webassembly.org/
 /// [Cargo-APK]: https://github.com/rust-mobile/cargo-apk
