279da0df08d2db03b4a6c064d3570af4160f33aa
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2025-05-09 15:48:03 +0200

    Removed shard: ./shader

--- crates/shader/src/error.rs
+++
@@ -1,6 +0,0 @@
-#[derive(Debug)]
-pub enum Error {
-    ShaderPreprocessor(shader_preprocessor::Error),
-    Texture(texture::Error),
-    IO(std::io::Error),
-}
--- crates/shader/src/variable.rs
+++
@@ -1,8 +0,0 @@
-use texture::Texture;
-use wgpu::Buffer;
-
-#[derive(Debug, PartialEq, Eq, Hash)]
-pub enum Variable {
-    Buffer(Buffer),
-    Texture(Texture),
-}
