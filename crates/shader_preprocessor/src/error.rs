1c825079068fa91739e0ab02ac8adfc09bccd0f8
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2025-05-09 15:48:04 +0200

    Removed shard: ./shader_preprocessor

--- crates/shader_preprocessor/src/error.rs
+++
@@ -1,9 +0,0 @@
-use std::{ffi::OsString, io};
-
-#[derive(Debug)]
-pub enum Error {
-    UnknownDirective { directive: String },
-    NonUTF8FileName { file_name: OsString },
-    IOError(io::Error),
-    PatternError(glob::PatternError),
-}
