8fad604aefd657e0f9c755f1550b73e97ec4518c
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2025-05-09 17:08:14 +0200

    Cleanup

--- Runtime/src/lib.rs
+++ Runtime/src/lib.rs
@@ -1,19 +1,14 @@
 pub mod app;
 pub mod cache;
-pub mod camera;
 pub mod change_list;
 pub mod element;
-pub mod ibl_brdf;
-pub mod instance;
 pub mod logging;
-pub mod material_shader;
 pub mod renderer;
-pub mod shader;
+pub mod resources;
 pub mod shader_preprocessor;
 pub mod transform;
 pub mod world;
 pub mod world_environment;
-pub mod resources;
 
 #[cfg(test)]
 pub mod wgpu_test_adapter;
