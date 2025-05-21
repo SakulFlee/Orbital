a87407635d293dedff68921f6037f900d37084e3
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2025-05-21 17:55:15 +0200

    Added more TODO labels

--- Runtime/src/element/world_change/element.rs
+++ Runtime/src/element/world_change/element.rs
@@ -4,10 +4,12 @@ use crate::element::Element;
 pub enum ElementChange {
     Spawn(Box<dyn Element + Send + Sync>),
     Despawn(String),
+    // TODO: Make singular
     AddLabels {
         element_label: String,
         new_labels: Vec<String>,
     },
+    // TODO: Make singular
     RemoveLabels {
         element_label: String,
         new_labels: Vec<String>,
