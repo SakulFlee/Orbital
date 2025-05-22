3f283b96a0a0d541856afd049c4125e78a222584
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2025-05-22 21:01:46 +0200

    Removed feature flags on Example regarding Renderer

--- Examples/Example/src/entrypoint.rs
+++ Examples/Example/src/entrypoint.rs
@@ -4,15 +4,6 @@ use orbital::{
     winit::{error::EventLoopError, event_loop::EventLoop},
 };
 
-#[cfg(feature = "non_caching_direct_renderer")]
-use orbital::renderer::NonCachingDirectRenderer;
-
-#[cfg(feature = "caching_direct_renderer")]
-use orbital::renderer::CachingDirectRenderer;
-
-#[cfg(feature = "caching_indirect_renderer")]
-use orbital::renderer::CachingIndirectRenderer;
-
 use crate::app::MyApp;
 
 pub const NAME: &str = "Orbital-Demo-Project";
@@ -26,31 +17,7 @@ pub fn entrypoint(event_loop_result: Result<EventLoop<()>, EventLoopError>) {
     app_settings.vsync_enabled = false;
     app_settings.name = NAME.to_string();
 
-    #[cfg(all(
-        feature = "caching_direct_renderer",
-        feature = "non_caching_direct_renderer",
-        feature = "caching_indirect_renderer"
-    ))]
-    compile_error!("Cannot enable all renderers at once!");
-    #[cfg(all(
-        feature = "non_caching_direct_renderer",
-        not(feature = "caching_direct_renderer"),
-        not(feature = "caching_indirect_renderer")
-    ))]
-    let app =
-        MyApp::<NonCachingDirectRenderer>::new(CacheSettings::default(), CacheSettings::default());
-    #[cfg(all(
-        feature = "caching_direct_renderer",
-        not(feature = "non_caching_direct_renderer"),
-        not(feature = "caching_indirect_renderer")
-    ))]
-    let app = MyApp::<CachingDirectRenderer>::new();
-    #[cfg(all(
-        feature = "caching_indirect_renderer",
-        not(feature = "non_caching_direct_renderer"),
-        not(feature = "caching_direct_renderer")
-    ))]
-    let app = MyApp::<CachingIndirectRenderer>::new();
+    let app = MyApp::new();
 
     AppRuntime::liftoff(event_loop, app_settings, app).expect("Runtime failure");
 }
