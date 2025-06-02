e52bc69ea538c391d11c14f3d5633a4508efd441
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2025-06-02 14:49:05 +0200

    Fixed Model unit test

--- Runtime/src/resources/model/tests.rs
+++ Runtime/src/resources/model/tests.rs
@@ -1,6 +1,9 @@
 use cgmath::{Point3, Vector2, Vector3};
 
-use crate::{resources::{BoundingBoxDescriptor, Mesh, MeshDescriptor, Vertex}, wgpu_test_adapter};
+use crate::{
+    resources::{Mesh, MeshDescriptor, Vertex},
+    wgpu_test_adapter,
+};
 
 #[test]
 fn realization() {
@@ -15,19 +18,35 @@ fn realization() {
             uv: Vector2::new(1.0, 2.0),
         }],
         indices: vec![0],
-        bounding_box: BoundingBoxDescriptor {
-            min: Point3 {
-                x: 1.0,
-                y: 2.0,
-                z: 3.0,
-            },
-            max: Point3 {
-                x: 4.0,
-                y: 5.0,
-                z: 6.0,
-            },
-        },
     };
 
     let _realization = Mesh::from_descriptor(&descriptor, &device, &queue);
+}
+
+#[test]
+fn bounding_box() {
+    let descriptor = MeshDescriptor {
+        vertices: vec![
+            Vertex {
+                position: Vector3::new(-5.0, -5.0, -5.0),
+                normal: Vector3::new(0.0, 0.0, 0.0),
+                tangent: Vector3::new(0.0, 0.0, 0.0),
+                bitangent: Vector3::new(0.0, 0.0, 0.0),
+                uv: Vector2::new(0.0, 0.0),
+            },
+            Vertex {
+                position: Vector3::new(5.0, 5.0, 5.0),
+                normal: Vector3::new(0.0, 0.0, 0.0),
+                tangent: Vector3::new(0.0, 0.0, 0.0),
+                bitangent: Vector3::new(0.0, 0.0, 0.0),
+                uv: Vector2::new(0.0, 0.0),
+            },
+        ],
+        indices: vec![0],
+    };
+
+    let bounding_box = descriptor.find_bounding_box();
+
+    assert_eq!(bounding_box.min, Point3::new(-5.0, -5.0, -5.0));
+    assert_eq!(bounding_box.max, Point3::new(5.0, 5.0, 5.0));
 }
