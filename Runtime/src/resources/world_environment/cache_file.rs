f09813ca9e15ef5736d7d313d8161a2cd8688d3e
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2025-06-02 17:12:06 +0200

    fmt

--- Runtime/src/resources/world_environment/cache_file.rs
+++ Runtime/src/resources/world_environment/cache_file.rs
@@ -1,11 +1,17 @@
-use std::{fs::{self, File}, io::{Read, Write}, path::Path};
+use std::{
+    fs::{self, File},
+    io::{Read, Write},
+    path::Path,
+};
 
 use cgmath::Vector2;
 use log::{debug, warn};
 use serde::{Deserialize, Serialize};
 use wgpu::{Device, Queue, TextureFormat, TextureUsages};
 
-use crate::resources::{WorldEnvironmentError, WorldEnvironmentDescriptor, Texture as OrbitalTexture};
+use crate::resources::{
+    Texture as OrbitalTexture, WorldEnvironmentDescriptor, WorldEnvironmentError,
+};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct CacheFile {
@@ -19,23 +25,27 @@ impl CacheFile {
         P: AsRef<Path>,
     {
         let mut file = File::open(path).map_err(WorldEnvironmentError::IO)?;
-    
+
         // Read sizes
         let mut size_buffer = [0u8; 8];
-        file.read_exact(&mut size_buffer).map_err(WorldEnvironmentError::IO)?;
+        file.read_exact(&mut size_buffer)
+            .map_err(WorldEnvironmentError::IO)?;
         let diffuse_size = u64::from_le_bytes(size_buffer);
         debug!("IBL Diffuse expected size in bytes: {}", diffuse_size);
-        
-        file.read_exact(&mut size_buffer).map_err(WorldEnvironmentError::IO)?;
+
+        file.read_exact(&mut size_buffer)
+            .map_err(WorldEnvironmentError::IO)?;
         let specular_size = u64::from_le_bytes(size_buffer);
         debug!("IBL Specular expected size in bytes: {}", specular_size);
-        
+
         // Read data
         let mut ibl_diffuse_data = vec![0u8; diffuse_size as usize];
         let mut ibl_specular_data = vec![0u8; specular_size as usize];
-        
-        file.read_exact(&mut ibl_diffuse_data).map_err(WorldEnvironmentError::IO)?;
-        file.read_exact(&mut ibl_specular_data).map_err(WorldEnvironmentError::IO)?;
+
+        file.read_exact(&mut ibl_diffuse_data)
+            .map_err(WorldEnvironmentError::IO)?;
+        file.read_exact(&mut ibl_specular_data)
+            .map_err(WorldEnvironmentError::IO)?;
 
         Ok(Self {
             ibl_diffuse_data,
@@ -43,36 +53,61 @@ impl CacheFile {
         })
     }
 
-    pub fn to_path<P>(&self, path: P) -> Result<(), WorldEnvironmentError> where P: AsRef<Path>, {
+    pub fn to_path<P>(&self, path: P) -> Result<(), WorldEnvironmentError>
+    where
+        P: AsRef<Path>,
+    {
         // Create parent folder(s) if they don't exist
         if let Some(parent) = path.as_ref().parent() {
             if !parent.exists() {
                 fs::create_dir_all(parent).map_err(WorldEnvironmentError::IO)?;
             }
         } else {
-            warn!("Path doesn't have a parent, the next step might fail to save the cache to disk!");
+            warn!(
+                "Path doesn't have a parent, the next step might fail to save the cache to disk!"
+            );
         }
 
         // Create the file if it doesn't exist, truncate if it does, and write self to it
         let mut file = File::create(path).map_err(WorldEnvironmentError::IO)?;
-        
+
         // Write sizes first
-        file.write_all(&(self.ibl_diffuse_data.len() as u64).to_le_bytes()).map_err(WorldEnvironmentError::IO)?;
-        file.write_all(&(self.ibl_specular_data.len() as u64).to_le_bytes()).map_err(WorldEnvironmentError::IO)?;
-        
+        file.write_all(&(self.ibl_diffuse_data.len() as u64).to_le_bytes())
+            .map_err(WorldEnvironmentError::IO)?;
+        file.write_all(&(self.ibl_specular_data.len() as u64).to_le_bytes())
+            .map_err(WorldEnvironmentError::IO)?;
+
         // Write actual data
-        file.write_all(&self.ibl_diffuse_data).map_err(WorldEnvironmentError::IO)?;
-        file.write_all(&self.ibl_specular_data).map_err(WorldEnvironmentError::IO)?;
+        file.write_all(&self.ibl_diffuse_data)
+            .map_err(WorldEnvironmentError::IO)?;
+        file.write_all(&self.ibl_specular_data)
+            .map_err(WorldEnvironmentError::IO)?;
 
         file.flush().map_err(WorldEnvironmentError::IO)?;
 
         Ok(())
     }
 
-    pub fn make_textures(&self, world_environment_descriptor: &WorldEnvironmentDescriptor, device: &Device, queue: &Queue) -> (OrbitalTexture, OrbitalTexture) {
+    pub fn make_textures(
+        &self,
+        world_environment_descriptor: &WorldEnvironmentDescriptor,
+        device: &Device,
+        queue: &Queue,
+    ) -> (OrbitalTexture, OrbitalTexture) {
         let (cube_face_size, specular_mip_level_count) = match world_environment_descriptor {
-            WorldEnvironmentDescriptor::FromFile { cube_face_size, path: _, sampling_type: _, custom_specular_mip_level_count: specular_mip_level_count } => (*cube_face_size, specular_mip_level_count.unwrap_or(1)),
-            WorldEnvironmentDescriptor::FromData { cube_face_size, data: _ , size: _, sampling_type : _, specular_mip_level_count } => (*cube_face_size, specular_mip_level_count.unwrap_or(1)),
+            WorldEnvironmentDescriptor::FromFile {
+                cube_face_size,
+                path: _,
+                sampling_type: _,
+                custom_specular_mip_level_count: specular_mip_level_count,
+            } => (*cube_face_size, specular_mip_level_count.unwrap_or(1)),
+            WorldEnvironmentDescriptor::FromData {
+                cube_face_size,
+                data: _,
+                size: _,
+                sampling_type: _,
+                specular_mip_level_count,
+            } => (*cube_face_size, specular_mip_level_count.unwrap_or(1)),
         };
 
         let ibl_diffuse_texture = OrbitalTexture::from_binary_data(
