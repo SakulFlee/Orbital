f4d7d54b935d8ed145dafad9252d77cb7f4509d8
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2025-06-06 22:26:58 +0200

    Added EnvironmentStore

---
+++ Runtime/src/world/store/environment.rs
@@ -0,0 +1,67 @@
+use std::error::Error;
+
+use log::warn;
+use wgpu::{Device, Queue};
+
+use crate::{
+    element::EnvironmentEvent,
+    resources::{WorldEnvironment, WorldEnvironmentDescriptor},
+};
+
+#[derive(Debug, Default)]
+pub struct EnvironmentStore {
+    world_environment: Option<WorldEnvironment>,
+    queued_descriptor: Option<WorldEnvironmentDescriptor>,
+}
+
+impl EnvironmentStore {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn queue_change(&mut self, descriptor: WorldEnvironmentDescriptor) {
+        if self.queued_descriptor.is_some() {
+            warn!("A WorldEnvironment change has already been queued and will be replaced! Old: {:?}, New: {:?}", &self.queued_descriptor.as_ref().unwrap(), descriptor);
+        }
+
+        self.queued_descriptor = Some(descriptor);
+    }
+
+    pub fn realize_and_cache(
+        &mut self,
+        device: &Device,
+        queue: &Queue,
+    ) -> Result<(), Box<dyn Error>> {
+        let descriptor = match self.queued_descriptor.take() {
+            Some(descriptor) => descriptor,
+            None => return Ok(()),
+        };
+
+        let realization = match WorldEnvironment::from_descriptor(&descriptor, device, queue) {
+            Ok(x) => x,
+            Err(e) => return Err(Box::new(e)),
+        };
+        self.world_environment = Some(realization);
+
+        Ok(())
+    }
+
+    pub fn world_environment(&self) -> Option<&WorldEnvironment> {
+        self.world_environment.as_ref()
+    }
+
+    pub fn clear(&mut self) {
+        self.world_environment = None;
+    }
+
+    pub fn handle_event(&mut self, environment_event: EnvironmentEvent) {
+        match environment_event {
+            EnvironmentEvent::Change {
+                descriptor,
+                enable_ibl, // TODO
+            } => {
+                self.queue_change(descriptor);
+            }
+        }
+    }
+}
--- Runtime/src/world/store/mod.rs
+++ Runtime/src/world/store/mod.rs
@@ -9,7 +9,14 @@ pub use model::*;
 mod camera;
 pub use camera::*;
 
+mod environment;
+pub use environment::*;
+
+
 mod error;
 pub use error::*;
 
 pub type Store<T> = HashMap<String, Arc<RwLock<T>>>;
+
+
+
