39d2f2fecc3e7d87dca5fa9e3bf752a2c2ef472c
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2025-06-16 01:31:41 +0200

    Using new entrypoint macro

--- Examples/Example/src/lib.rs
+++ Examples/Example/src/lib.rs
@@ -1,6 +1,28 @@
+use orbital::{
+    app::{AppRuntime, AppSettings},
+    logging::{self, error, info},
+    make_main,
+    winit::{error::EventLoopError, event_loop::EventLoop},
+};
+
 pub mod app;
+use app::*;
 
-pub mod entrypoint;
+pub const NAME: &str = "Orbital-Demo-Project";
 
-#[cfg(target_os = "android")]
-pub mod main_android;
+pub fn entrypoint(event_loop_result: Result<EventLoop<()>, EventLoopError>) {
+    logging::init();
+
+    let event_loop = event_loop_result.expect("Event Loop failure");
+
+    let mut app_settings = AppSettings::default();
+    app_settings.vsync_enabled = false;
+    app_settings.name = NAME.to_string();
+
+    match AppRuntime::<MyApp>::liftoff(event_loop, app_settings) {
+        Ok(()) => info!("Cleanly exited!"),
+        Err(e) => error!("Runtime failure: {e:?}"),
+    }
+}
+
+make_main!(entrypoint);
