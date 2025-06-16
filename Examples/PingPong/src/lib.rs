4600076cbeac178337b0289af2151110116ae53c
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2025-06-17 00:05:59 +0200

    Added PingPong example

---
+++ Examples/PingPong/src/lib.rs
@@ -0,0 +1,36 @@
+use orbital::{
+    app::{AppRuntime, AppSettings},
+    logging::{self, error, info},
+    make_main,
+    winit::{error::EventLoopError, event_loop::EventLoop},
+};
+
+use orbital::app::standard::StandardApp;
+
+mod element;
+use element::*;
+
+pub const NAME: &str = "Orbital-Demo-Project: PingPong";
+
+pub fn entrypoint(event_loop_result: Result<EventLoop<()>, EventLoopError>) {
+    logging::init();
+
+    let event_loop = event_loop_result.expect("Event Loop failure");
+
+    let mut app_settings = AppSettings::default();
+    app_settings.vsync_enabled = false;
+    app_settings.name = NAME.to_string();
+
+    // Create both a Ping and a Pong element to demonstrate message passing
+    let app = StandardApp::with_initial_elements(vec![
+        Box::new(PingPongElement::new(true)),  // Ping
+        Box::new(PingPongElement::new(false)), // Pong
+    ]);
+
+    match AppRuntime::liftoff(event_loop, app_settings, app) {
+        Ok(()) => info!("Cleanly exited!"),
+        Err(e) => error!("Runtime failure: {e:?}"),
+    }
+}
+
+make_main!(entrypoint);
