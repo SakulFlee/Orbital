c9d37894f5e0feac3890f09c5fdc15b39d1886c5
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2025-06-17 00:26:01 +0200

    Fixed PingPongElement

--- Examples/PingPong/src/element.rs
+++ Examples/PingPong/src/element.rs
@@ -1,9 +1,7 @@
 use orbital::{
     async_trait::async_trait,
-    hashbrown::HashMap,
-    input::InputState,
-    variant::Variant,
-    world_old::{Element, ElementRegistration, Message, WorldChange},
+    element::{Element, ElementEvent, ElementRegistration, Event, Message, Target, Variant},
+    app::input::InputState,
 };
 
 #[derive(Debug)]
@@ -28,13 +26,15 @@ impl Element for PingPongElement {
         let element_registration = ElementRegistration::new("PingPong");
 
         if self.is_ping {
+            let mut message = Message::new(
+                Self::LABEL_PING.to_string(),
+                Target::Element { label: Self::LABEL_PONG.to_string() }
+            );
+            message.add_content("payload".to_string(), Variant::Boolean(self.is_ping));
+
             element_registration
                 .with_additional_label(Self::LABEL_PING)
-                .with_initial_world_change(WorldChange::SendMessage(Message::new_from_message(
-                    Self::LABEL_PING.into(),
-                    Self::LABEL_PONG.into(),
-                    HashMap::from_iter(vec![("payload".into(), Variant::Boolean(self.is_ping))]),
-                )))
+                .with_initial_world_change(Event::Element(ElementEvent::SendMessage(message)))
         } else {
             element_registration.with_additional_label(Self::LABEL_PONG)
         }
@@ -45,7 +45,7 @@ impl Element for PingPongElement {
         _delta_time: f64,
         _input_state: &InputState,
         messages_option: Option<Vec<Message>>,
-    ) -> Option<Vec<WorldChange>> {
+    ) -> Option<Vec<Event>> {
         if let Some(messages) = messages_option {
             for message in messages {
                 if let Some(payload) = message.get("payload") {
@@ -53,31 +53,27 @@ impl Element for PingPongElement {
                         if *is_ping && !self.is_ping {
                             // Disabled to prevent log spam!
                             // debug!("Ping Received! Sending Pong back.");
-                            return Some(vec![WorldChange::SendMessage(
-                                Message::new_from_message(
-                                    Self::LABEL_PONG.into(),
-                                    Self::LABEL_PING.into(),
-                                    HashMap::from_iter(vec![(
-                                        "payload".into(),
-                                        Variant::Boolean(self.is_ping),
-                                    )]),
-                                ),
-                            )]);
+                            let mut message = Message::new(
+                                Self::LABEL_PONG.to_string(),
+                                Target::Element { label: Self::LABEL_PING.to_string() }
+                            );
+                            message.add_content("payload".to_string(), Variant::Boolean(self.is_ping));
+
+                            return Some(vec![Event::Element(ElementEvent::SendMessage(message))]);
                         } else if !*is_ping && self.is_ping {
                             // Disabled to prevent log spam!
                             // debug!("Pong Received! Sending Ping back.");
-                            return Some(vec![WorldChange::SendMessage(
-                                Message::new_from_message(
-                                    Self::LABEL_PING.into(),
-                                    Self::LABEL_PONG.into(),
-                                    HashMap::from_iter(vec![(
-                                        "payload".into(),
-                                        Variant::Boolean(self.is_ping),
-                                    )]),
-                                ),
-                            )]);
+                            let mut message = Message::new(
+                                Self::LABEL_PING.to_string(),
+                                Target::Element { label: Self::LABEL_PONG.to_string() }
+                            );
+                            message.add_content("payload".to_string(), Variant::Boolean(self.is_ping));
+
+                            return Some(vec![Event::Element(ElementEvent::SendMessage(message))]);
                         } else {
-                            panic!("Received Ping payload as Ping element, or, Pong payload as Pong element. This should never happen!");
+                            panic!(
+                                "Received Ping payload as Ping element, or, Pong payload as Pong element. This should never happen!"
+                            );
                         }
                     }
                 }
