f4c520252787cbb971bcecc974b303b156e6c109
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2025-06-26 22:38:35 +0200

    Bugfix: Runtime logs would be incremented and renamed upon each logging::init() call
    Signed-off-by: @SakulFlee | Lukas Weber <dev@sakul-flee.de>

--- Runtime/src/logging/mod.rs
+++ Runtime/src/logging/mod.rs
@@ -1,6 +1,6 @@
-use std::{fs, path::Path, time::SystemTime};
-
 pub use log::*;
+use std::sync::Once;
+use std::{fs, path::Path, time::SystemTime};
 
 #[cfg(target_os = "android")]
 pub fn init() {
@@ -11,66 +11,69 @@ pub fn init() {
 
 #[cfg(not(target_os = "android"))]
 pub fn init() {
-    let default_log_level = if cfg!(debug_assertions) {
-        LevelFilter::Debug
-    } else {
-        LevelFilter::Info
-    };
+    static ONCE: Once = Once::new();
+    ONCE.call_once(|| {
+        let default_log_level = if cfg!(debug_assertions) {
+            LevelFilter::Debug
+        } else {
+            LevelFilter::Info
+        };
 
-    const START: u32 = 0;
-    const END: u32 = 4;
+        const START: u32 = 0;
+        const END: u32 = 4;
 
-    for i in (START..=END).rev() {
-        let log_file = format!("game-{}.log", i);
-        let path = Path::new(&log_file);
+        for i in (START..=END).rev() {
+            let log_file = format!("game-{}.log", i);
+            let path = Path::new(&log_file);
 
-        if path.exists() {
-            if i == END {
-                fs::remove_file(path).expect("failed removing last index log file");
-            } else {
-                let next_log_file = format!("game-{}.log", i + 1);
+            if path.exists() {
+                if i == END {
+                    fs::remove_file(path).expect("failed removing last index log file");
+                } else {
+                    let next_log_file = format!("game-{}.log", i + 1);
 
-                fs::rename(path, next_log_file).expect("failed renaming log file to next index");
+                    fs::rename(path, next_log_file).expect("failed renaming log file to next index");
+                }
             }
         }
-    }
 
-    if let Err(e) = fern::Dispatch::new()
-        // Setup formation
-        .format(|out, message, record| {
-            out.finish(format_args!(
-                "[{} {} {}] {}",
-                humantime::format_rfc3339_seconds(SystemTime::now()),
-                record.level(),
-                record.target(),
-                message
-            ))
-        })
-        .chain(
-            fern::Dispatch::new()
-                // Default level to accept
-                .level(default_log_level)
-                // WGPU Overwrite
-                .level_for("wgpu_core", LevelFilter::Warn)
-                .level_for("wgpu_hal", LevelFilter::Warn)
-                .level_for("naga", LevelFilter::Warn)
-                // Write to StdOut
-                .chain(std::io::stdout())
-                .chain(
-                    fern::log_file(format!("game-{}.log", START))
-                        .expect("failed building file log"),
-                ),
-        )
-        // Apply as global logger!
-        .apply()
-    {
-        error!(
+        if let Err(e) = fern::Dispatch::new()
+            // Setup formation
+            .format(|out, message, record| {
+                out.finish(format_args!(
+                    "[{} {} {}] {}",
+                    humantime::format_rfc3339_seconds(SystemTime::now()),
+                    record.level(),
+                    record.target(),
+                    message
+                ))
+            })
+            .chain(
+                fern::Dispatch::new()
+                    // Default level to accept
+                    .level(default_log_level)
+                    // WGPU Overwrite
+                    .level_for("wgpu_core", LevelFilter::Warn)
+                    .level_for("wgpu_hal", LevelFilter::Warn)
+                    .level_for("naga", LevelFilter::Warn)
+                    // Write to StdOut
+                    .chain(std::io::stdout())
+                    .chain(
+                        fern::log_file(format!("game-{}.log", START))
+                            .expect("failed building file log"),
+                    ),
+            )
+            // Apply as global logger!
+            .apply()
+        {
+            error!(
             "Failure creating logger. This is commonly due to a logger already being initialized beforehand. Error: {}",
             e
         );
-    }
+        }
 
-    info!("Logger initialized at max level set to {}", max_level());
+        info!("Logger initialized at max level set to {}", max_level());
+    });
 }
 
 #[cfg(not(target_os = "android"))]
