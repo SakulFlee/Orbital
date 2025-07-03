e92a2593bf6eb8bcf80ddc9190b7db46521a4519
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2025-07-03 21:06:21 +0200

    Starting with tests for GLTFImporter

--- Runtime/src/loader/gltf/tests.rs
+++ Runtime/src/loader/gltf/tests.rs
@@ -4,19 +4,32 @@ use crate::loader::gltf::{GltfImport, GltfImportTask, GltfImportType, GltfImport
 use crate::logging;
 
 #[test]
-fn test_load_camera() {
+fn load_gltf() {
     logging::test_init();
-    
+
     let task = GltfImportTask {
-        file: "../Assets/Models/TestScene.gltf".to_string(),
-        import: GltfImport::Specific(vec![SpecificGltfImport {
-            label: "TestCamera".to_string(),
-            import_type: GltfImportType::Camera
-        }]),
+        file: "../Examples/SharedAssets/Models/TestScene.gltf".to_string(),
+        import: GltfImport::WholeFile,
     };
-    
+
     let x = GltfImporter::import(task);
-    let y = block_on(x);
-    debug!("{:?}", y);
-    assert!(y.is_ok());
+    let (_models, _cameras, errors) = block_on(x);
+    debug!("{:?}", errors);
+    assert!(errors.is_empty());
+}
+
+
+#[test]
+fn load_glb() {
+    logging::test_init();
+
+    let task = GltfImportTask {
+        file: "../Examples/SharedAssets/Models/TestScene.glb".to_string(),
+        import: GltfImport::WholeFile,
+    };
+
+    let x = GltfImporter::import(task);
+    let (_models, _cameras, errors) = block_on(x);
+    debug!("{:?}", errors);
+    assert!(errors.is_empty());
 }
