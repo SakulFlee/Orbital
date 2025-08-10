2718001abd86a65459490b440f4fd642ff5a89d3
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2025-08-10 22:11:32 +0200

    Enabled Damaged Helmet example

--- Examples/damaged_helmet/src/element.rs
+++ Examples/damaged_helmet/src/element.rs
@@ -1,46 +1,23 @@
-// use orbital::{
-//     app::input::InputState,
-//     async_trait::async_trait,
-//     cgmath::Vector3,
-//     element::{Element, ElementRegistration, Message},
-//     loader::{GLTFLoader, GLTFWorkerMode},
-//     transform::Transform,
-//     world_old::WorldChange,
-// };
-//
-// #[derive(Debug)]
-// pub struct DamagedHelmet;
-//
-// impl DamagedHelmet {
-//     const FILE_NAME: &'static str = "Assets/Models/DamagedHelmet.glb";
-// }
-//
-// #[async_trait]
-// impl Element for DamagedHelmet {
-//     fn on_registration(&self) -> ElementRegistration {
-//         ElementRegistration::new(Self::FILE_NAME).with_initial_world_change(
-//             WorldChange::EnqueueLoader(Box::new(GLTFLoader::new(
-//                 Self::FILE_NAME,
-//                 GLTFWorkerMode::LoadEverything,
-//                 Some(Transform {
-//                     position: Vector3::new(0.0, 0.0, 5.0),
-//                     ..Default::default()
-//                 }),
-//             ))),
-//         )
-//     }
-//
-//     async fn on_update(
-//         &mut self,
-//         delta_time: f64,
-//         _input_state: &InputState,
-//         _messages: Option<Vec<Message>>,
-//     ) -> Option<Vec<WorldChange>> {
-//         // TODO
-//         // Some(vec![WorldChange::ApplyTransformModel(
-//         //     "mesh_helmet_LP_13930damagedHelmet".into(),
-//         //     Transform::only_position(Vector3::new(0.0, 0.0, 1.0 * delta_time as f32)),
-//         // )])
-//         None
-//     }
-// }
+use orbital::{
+    async_trait::async_trait,
+    element::{Element, ElementRegistration, Event, WorldEvent},
+    importer::{ImportTask, gltf::GltfImport},
+};
+
+#[derive(Debug)]
+pub struct DamagedHelmet;
+impl DamagedHelmet {
+    const FILE_NAME: &'static str = "Assets/Models/DamagedHelmet.glb";
+}
+
+#[async_trait]
+impl Element for DamagedHelmet {
+    fn on_registration(&self) -> ElementRegistration {
+        ElementRegistration::new(Self::FILE_NAME).with_initial_world_change(Event::World(
+            WorldEvent::Import(ImportTask::Gltf {
+                file_path: Self::FILE_NAME.to_string(),
+                task: GltfImport::WholeFile,
+            }),
+        ))
+    }
+}
