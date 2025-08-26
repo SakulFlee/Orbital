7df50e7a3c513a400840ed8a018c9acdb157e339
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2025-08-27 01:32:51 +0200

    Added all entries to World/Engine BindGroup

--- Runtime/src/resources/material_shader/engine_bind_group_layout.rs
+++ Runtime/src/resources/material_shader/engine_bind_group_layout.rs
@@ -1,6 +1,6 @@
 use wgpu::{
     BindGroupLayout, BindGroupLayoutDescriptor, BindGroupLayoutEntry, BindingType,
-    BufferBindingType, Device, ShaderStages,
+    BufferBindingType, Device, SamplerBindingType, ShaderStages, TextureViewDimension,
 };
 
 pub struct EngineBindGroupLayout;
@@ -18,6 +18,57 @@ impl EngineBindGroupLayout {
                         has_dynamic_offset: false,
                         min_binding_size: None,
                     },
+                    count: None,
+                },
+                // IBL Diffuse
+                BindGroupLayoutEntry {
+                    binding: 1,
+                    visibility: ShaderStages::all(),
+                    ty: BindingType::Texture {
+                        sample_type: wgpu::TextureSampleType::Float { filterable: false },
+                        view_dimension: TextureViewDimension::D2,
+                        multisampled: false,
+                    },
+                    count: None,
+                },
+                BindGroupLayoutEntry {
+                    binding: 2,
+                    visibility: ShaderStages::all(),
+                    ty: BindingType::Sampler(SamplerBindingType::NonFiltering),
+                    count: None,
+                },
+                // IBL Specular
+                BindGroupLayoutEntry {
+                    binding: 3,
+                    visibility: ShaderStages::all(),
+                    ty: BindingType::Texture {
+                        sample_type: wgpu::TextureSampleType::Float { filterable: false },
+                        view_dimension: TextureViewDimension::D2,
+                        multisampled: false,
+                    },
+                    count: None,
+                },
+                BindGroupLayoutEntry {
+                    binding: 4,
+                    visibility: ShaderStages::all(),
+                    ty: BindingType::Sampler(SamplerBindingType::NonFiltering),
+                    count: None,
+                },
+                // IBL BRDF
+                BindGroupLayoutEntry {
+                    binding: 5,
+                    visibility: ShaderStages::all(),
+                    ty: BindingType::Texture {
+                        sample_type: wgpu::TextureSampleType::Float { filterable: false },
+                        view_dimension: TextureViewDimension::D2,
+                        multisampled: false,
+                    },
+                    count: None,
+                },
+                BindGroupLayoutEntry {
+                    binding: 6,
+                    visibility: ShaderStages::all(),
+                    ty: BindingType::Sampler(SamplerBindingType::NonFiltering),
                     count: None,
                 },
             ],
