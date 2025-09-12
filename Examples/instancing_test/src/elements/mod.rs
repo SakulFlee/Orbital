2caf77873801b8e1596113320f428c9227202596
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2025-09-12 22:39:50 +0200

    Added InstanceTest example

---
+++ Examples/instancing_test/src/elements/mod.rs
@@ -0,0 +1,5 @@
+mod world_environment;
+pub use world_environment::*;
+
+mod instancing_test;
+pub use instancing_test::*;
---
+++ Examples/instancing_test/src/main.rs
@@ -0,0 +1,5 @@
+use example_instancing_test::entrypoint;
+
+use orbital::make_desktop_main;
+
+make_desktop_main!(entrypoint);
