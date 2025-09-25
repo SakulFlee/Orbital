4803bd420966dfd2fc173f7ddbe5c6e2e46a379f
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2025-09-25 22:31:00 +0200

    Cleanup

--- Runtime/src/element/event/element.rs
+++ Runtime/src/element/event/element.rs
@@ -4,12 +4,10 @@ use crate::element::{Element, Message};
 pub enum ElementEvent {
     Spawn(Box<dyn Element + Send + Sync>),
     Despawn(String),
-    // TODO: Make singular
     AddLabels {
         element_label: String,
         new_labels: Vec<String>,
     },
-    // TODO: Make singular
     RemoveLabels {
         element_label: String,
         labels_to_be_removed: Vec<String>,
