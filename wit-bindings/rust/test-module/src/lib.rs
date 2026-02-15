2674bc5785b1b6fffd13f1057527fe604d26e606
Lukas Weber | @SakulFlee <dev@sakul-flee.de>
2026-02-15 02:22:26 +0100

    Added WIT TestModule for Rust

---
+++ wit-bindings/rust/test-module/src/lib.rs
@@ -0,0 +1,16 @@
+wit_bindgen::generate!({
+    world: "orbital",
+    path: "../../../WIT/"
+});
+
+use crate::exports::orbital::core::{commands::CommandBuffer, module::Guest};
+
+pub struct TestModule;
+
+impl Guest for TestModule {
+    fn startup() -> Option<CommandBuffer> {
+        println!("'Hello World!' from WIT TestModule: Rust");
+
+        None
+    }
+}
