e2a444a40d283d3416a43fb8a9d3c5fd51e39cfd
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-05-31 01:19:01 +0200

    Turned ComponentStore into proper SparseSet

--- Crates/ecs/src/component/store.rs
+++ Crates/ecs/src/component/store.rs
@@ -2,51 +2,75 @@ use crate::EntityIdType;
 
 #[derive(Debug)]
 pub struct ComponentStore<T> {
-    pub entity_map: Vec<Option<EntityIdType>>,
+    /// [Entity ID] -> [Component Index]
+    /// Maps Entity ID to Component index in a sparse set.
+    /// Used for accessing a Component of a given Entity identified by an ID.
+    pub sparse: Vec<Option<EntityIdType>>,
+    /// [Component Index] -> [Entity ID]
+    /// Maps Component index back to Entity IDs in a dense set.
+    /// Used for detaching/removing components to have a "reverse lookup" available.
+    pub dense: Vec<EntityIdType>,
+    /// [Component Index] -> [Component]
+    /// Stores the actual Components in a dense set.
     pub components: Vec<T>,
 }
 
 impl<T> ComponentStore<T> {
     pub fn new() -> Self {
         Self {
-            entity_map: Vec::new(),
+            sparse: Vec::new(),
+            dense: Vec::new(),
             components: Vec::new(),
         }
     }
 
     pub fn attach(&mut self, entity_id: EntityIdType, component: T) {
+        // Aquire next index
         let next_index = self.components.len();
-        self.components.push(component);
 
-        if entity_id >= self.entity_map.len() {
-            self.entity_map.resize(entity_id + 1, None);
+        // Push to dense arrays
+        self.components.push(component);
+        self.dense.push(entity_id);
+
+        // Update sparse map
+        if entity_id >= self.sparse.len() {
+            self.sparse.resize(entity_id + 1, None);
         }
-        self.entity_map[entity_id] = Some(next_index);
+        self.sparse[entity_id] = Some(next_index);
     }
 
     pub fn detach(&mut self, entity_id: EntityIdType) -> Option<T> {
-        if entity_id >= self.entity_map.len() {
+        if entity_id >= self.sparse.len() {
             // Cannot logically be present
             return None;
         }
 
-        let component_index = self.entity_map[entity_id]?;
-        if component_index > 0 {
-            self.entity_map[entity_id] = None;
-            let component = self.components.swap_remove(component_index);
-            return Some(component);
+        // Retrieve component access, return None if it doesn't exist
+        let component_index = self.sparse[entity_id]?;
+
+        // Clear sparse index
+        self.sparse[entity_id] = None;
+
+        // Swap remove on both dense arrays
+        let component = self.components.swap_remove(component_index);
+        self.dense.swap_remove(component_index);
+
+        if component_index < self.components.len() {
+            let moved_entity = self.dense[component_index];
+
+            self.sparse[moved_entity] = Some(component_index);
         }
 
-        None
+        Some(component)
     }
 
     pub fn get_component(&self, entity_id: EntityIdType) -> Option<&T> {
-        if entity_id >= self.entity_map.len() {
+        if entity_id >= self.sparse.len() {
             // Cannot logically be present
             return None;
         }
 
-        let component_index = self.entity_map[entity_id]?;
+        let component_index = self.sparse[entity_id]?;
         self.components.get(component_index)
     }
 }
