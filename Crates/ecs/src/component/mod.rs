018b600a0b801ea47325fe79b81fd8e910a07c13
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-05-30 04:26:13 +0200

    Added Component::make_store

--- Crates/ecs/src/component/mod.rs
+++ Crates/ecs/src/component/mod.rs
@@ -4,14 +4,14 @@ pub use store::*;
 mod world_store;
 pub use world_store::*;
 
-use std::{
-    any::{Any, TypeId},
-    fmt::Debug,
-};
+use std::{any::Any, fmt::Debug};
 
 pub trait Component: Any + Debug {
-    fn type_id(&self) -> TypeId {
-        Any::type_id(self)
+    fn make_store(&self) -> ComponentStore<Self>
+    where
+        Self: Sized,
+    {
+        ComponentStore::new()
     }
 }
 
