35242ab9939d7fb8456f1ced1b531107f61b4646
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-07-04 21:31:04 +0200

    Updated Woodpecker CI and GitHub Actions
    Signed-off-by: @SakulFlee | Lukas Weber <dev@sakul-flee.de>

--- flake.nix
+++ flake.nix
@@ -15,8 +15,6 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
 
-        # latest = nightly
-        # stable = stable
         rustToolchain = fenix.packages.${system}.latest.withComponents [
           "cargo"
           "clippy"
@@ -25,50 +23,63 @@
           "rustfmt"
           "rust-analyzer"
         ];
+
+        commonNativeBuildInputs = with pkgs; [
+          rustToolchain
+          pkgs.gcc
+          pkgs.binutils
+          pkgs.gnumake
+          pkgs.cargo-flamegraph
+          pkgs.cargo-criterion
+          pkgs.pkg-config
+          pkgs.blender
+        ];
+
+        commonBuildInputs = with pkgs; [
+          pkgs.glibc.dev
+          pkgs.systemd
+          wayland
+          libxkbcommon
+          vulkan-loader
+        ];
+
+        commonShellHook = ''
+          export RUST_SRC_PATH="${rustToolchain}/lib/rustlib/src/rust/library"
+          export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [ pkgs.wayland pkgs.libxkbcommon pkgs.vulkan-loader ]}"
+        '';
       in
       {
         devShells.default = pkgs.mkShell {
-          nativeBuildInputs = [
-            # Fenix toolchain
-            rustToolchain
-            
-            # Build tools
-            pkgs.gcc
-            pkgs.binutils 
-            pkgs.gnumake 
+          nativeBuildInputs = commonNativeBuildInputs;
 
-            # Additional Rust tools
-            pkgs.cargo-flamegraph
-            pkgs.cargo-criterion
+          buildInputs = commonBuildInputs;
 
-            # Build Dependencies
-            pkgs.pkg-config
-            pkgs.blender
-          ];
-
-          # Runtime dependencies
-          buildInputs = with pkgs; [
-            pkgs.glibc.dev
-            pkgs.systemd # provides 'libudev'
-
-            wayland          # libwayland-client.so
-            libxkbcommon     # often needed with wayland
-            vulkan-loader    # needed for wgpu
-          ];
-
-
-          shellHook = ''
-            # Point rust-analyzer directly to the fenix toolchain standard library source
-            export RUST_SRC_PATH="${rustToolchain}/lib/rustlib/src/rust/library"
-
-            # Fix for gLibC not finding gcc compiler
+          shellHook = commonShellHook + ''
             mkdir -p .cargo/bin
             ln -sf "$(which gcc)" .cargo/bin/x86_64-linux-gnu-gcc
             export PATH="$PWD/.cargo/bin:$PATH"
             export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER="$(which gcc)"
+          '';
+        };
 
-            # To find libraries at runtime
-            export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [ pkgs.wayland pkgs.libxkbcommon pkgs.vulkan-loader ]}"
+        devShells.aarch64-cross = pkgs.mkShell {
+          nativeBuildInputs = commonNativeBuildInputs ++ [
+            pkgs.pkgsCross.aarch64-multiplatform.buildPackages.stdenv.cc
+            pkgs.pkgsCross.aarch64-multiplatform.buildPackages.pkg-config
+          ];
+
+          buildInputs = with pkgs.pkgsCross.aarch64-multiplatform; [
+            glibc.dev
+            systemd
+            wayland
+            libxkbcommon
+            vulkan-loader
+          ];
+
+          shellHook = commonShellHook + ''
+            export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER="${pkgs.pkgsCross.aarch64-multiplatform.stdenv.cc}/bin/aarch64-unknown-linux-gnu-cc"
+            export CC_aarch64_unknown_linux_gnu="${pkgs.pkgsCross.aarch64-multiplatform.stdenv.cc}/bin/aarch64-unknown-linux-gnu-cc"
+            export PKG_CONFIG_ALLOW_CROSS=1
           '';
         };
       });
