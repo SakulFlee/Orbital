a4e5a69d818ef0bddc2d68062d40b3258b0d6075
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-09-10 23:11:13 +0200

    Remove prelude module declaration and re-export

--- Crates/orbital_shader_preprocessor/src/lib.rs
+++ Crates/orbital_shader_preprocessor/src/lib.rs
@@ -2,14 +2,12 @@ mod builder;
 mod error;
 mod library;
 mod node;
-mod prelude;
 mod registry;
 
 pub use builder::*;
 pub use error::ShaderPreprocessorError;
 pub use library::*;
 pub use node::*;
-pub use prelude::*;
 pub use registry::*;
 
 #[cfg(test)]
